e5010a9de30272104caca1babca94022e8702649
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2020-11-12 13:51:26 +0900

    運用系処理追加

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 非稼働分析 デモ用
+# H-One プレスラインシステム
 ## WebApp
 ### frontend
 Vue.js 3  
@@ -6,11 +6,7 @@ Vue.js 3
 ### backend
 Flask  
 
-## Script
-### data_generator
-ダミーのセンサーデータをJSONファイルで生成するスクリプト。  
-sin(x)の値を変位値とする。また、末尾の5サンプルは変位値0の異常データとしている。  
-データ数Nはスクリプトを書き換えること。  
+## Scripts
 
-### data_recorder
-data_generatorが生成したJSONファイルを読み込み、Elasticsearchに格納するスクリプト  
+
+## notebooks
--- notebooks/elastic_manager.py
+++ notebooks/elastic_manager.py
@@ -1,11 +1,12 @@
 from elasticsearch import Elasticsearch
+import pandas as pd
 
 
 class ElasticManager:
     es = Elasticsearch(hosts="localhost:9200", http_auth=('elastic', 'P@ssw0rd12345'), timeout=50000)
 
     @classmethod
-    def show_indices(cls, show_all_index: bool = False) -> None:
+    def show_indices(cls, show_all_index: bool = False) -> pd.DataFrame:
         '''
          インデックス一覧を表示する。
             Args:
@@ -20,15 +21,105 @@ class ElasticManager:
                 index=['rawdata-*', 'shots-*', 'meta-*'], v=True, h=['index', 'docs.count', 'store.size'], bytes='kb'
             ).splitlines()
 
-        for index in indices:
-            print(index)
+        indices_list = [x.split() for x in indices]
 
-    def delete_index(self) -> None:
-        pass
+        df = pd.DataFrame(indices_list[1:], columns=indices_list[0])
 
-    def delete_documents(self) -> None:
-        pass
+        return df
 
+    @classmethod
+    def delete_index(cls, index: str) -> None:
+        is_valid_index, message = ElasticManager.__check_index(index)
+
+        if not is_valid_index:
+            print(message)
+            return
+
+        result = cls.es.indices.delete(index=index)
+
+        if result['acknowledged']:
+            print(f"{index}を削除しました。")
+        else:
+            print(f"エラー：{index}を削除できませんでした。")
+
+    @classmethod
+    def delete_data_by_seq_num(cls, index: str, start: int, end: int) -> None:
+        is_valid_index, message = ElasticManager.__check_index(index)
+
+        if not is_valid_index:
+            print(message)
+            return
+
+        if start > end:
+            print(f"end({end})はstart({start})より大きい値を設定してください。")
+            return
+
+        if start < 0 or end < 0:
+            print("連番の値が不正です。")
+            return
+
+        body = {
+            "query": {
+                "range": {
+                    "sequential_number": {
+                        "gte": start,
+                        "lte": end
+                    }
+                }
+            }
+        }
+
+        result = cls.es.delete_by_query(index=index, body=body, refresh=True)
+
+        if len(result["failures"]) != 0:
+            print("エラー：データ削除に失敗しました。")
+            for failure in result["failures"]:
+                print(failure)
+            return
+
+        print(f"データを{result['deleted']}件削除しました。")
+
+    @classmethod
+    def delete_data_by_shot_num(cls, index: str, shot_number: int) -> None:
+        is_valid_index, message = ElasticManager.__check_index(index)
+
+        if not is_valid_index:
+            print(message)
+            return
+
+        if shot_number < 0:
+            print("ショット番号が不正です。")
+
+        query = {
+            "term": {
+                "shot_number": shot_number
+            }
+        }
+
+        result = cls.es.delete_by_query(index=index, body=query, refresh=True)
+
+        if len(result["failures"]) != 0:
+            print("エラー：データ削除に失敗しました。")
+            for failure in result["failures"]:
+                print(failure)
+            return
+
+        print(f"データを{result['deleted']}件削除しました。")
+
+    @classmethod
+    def __check_index(cls, index):
+        if index == '':
+            message = "エラー：インデックスを指定してください。"
+            return False, message
+
+        if not cls.es.indices.exists(index=index):
+            message = f"エラー：{index}が存在しません。"
+            return False, message
+
+        return True, ""
 
 if __name__ == '__main__':
-    ElasticManager.show_indices()
+    pass
+    # ElasticManager.show_indices()
+    # ElasticManager.delete_index('rawdata-20201112-*')
+    ElasticManager.delete_data_by_seq_num('rawdata-20201112-1', 0, 10)
