c8dd2500c0ba08737d0e339cb8fd875566ada8d2
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2020-12-09 09:25:20 +0000

    ショット切り出し処理をメソッド化

--- utils/data_importer.py
+++ utils/data_importer.py
@@ -58,7 +58,6 @@ class DataImporter:
     ) -> None:
         """
         csvファイルを一定行数に読み取り、ショット切り出しおよび保存を行う。
-        ショット切り出し処理。生データの変位値を参照し、ショット対象となるデータのみをリストに含めて返す。
 
         Args:
             rawdata_filename: 生データcsvのファイル名
@@ -68,6 +67,47 @@ class DataImporter:
             num_of_process: 並列処理のプロセス数
         """
 
+        CHUNKSIZE: Final = 10_000_000
+        TAIL_SIZE: Final = 1_000
+
+        cols = ("load01", "load02", "load03", "load04", "displacement")
+        current_df_tail: DataFrame = pd.DataFrame(index=[], columns=cols)
+
+        now = datetime.now()
+
+        # chunksize毎に処理
+        for loop_count, rawdata_df in enumerate(pd.read_csv(rawdata_filename, chunksize=CHUNKSIZE, names=cols)):
+            # スループット表示
+            if loop_count != 0:
+                processed_count: int = loop_count * CHUNKSIZE
+                throughput_counter(processed_count, now)
+
+            # chunk開始直後にショットを検知した場合、荷重開始点を含めるためにN件遡る。
+            # そのためのデータをprevious_df_tail（1つ前のchunkの末尾）とcurrent_df_tail（現在のchunkの末尾）に保持しておく。
+            previous_df_tail, current_df_tail = self._backup_df_tail(current_df_tail, rawdata_df, TAIL_SIZE)
+
+            # chunk内のサンプルを1つずつ確認し、ショット切り出し
+            shots: list = self._cut_out_shot(
+                previous_df_tail, rawdata_df, start_displacement, end_displacement, TAIL_SIZE
+            )
+
+            # 切り出されたショットデータをElasticsearchに書き出し
+            if len(shots) > 0:
+                ElasticManager.multi_process_bulk(shots, shots_index, num_of_process, 5000)
+                shots = []
+
+        logger.info("Cut_off finished.")
+
+    def _cut_out_shot(
+        self,
+        previous_df_tail: DataFrame,
+        rawdata_df: DataFrame,
+        start_displacement: float,
+        end_displacement: float,
+        TAIL_SIZE,
+    ) -> list:
+        """ ショット切り出し処理。生データの変位値を参照し、ショット対象となるデータのみをリストに含めて返す。 """
+
         is_shot_section: bool = False  # ショット内か否かを判別する
         is_target_of_cut_off: bool = False  # ショットの内、切り出し対象かを判別する
         sequential_number: int = 0
@@ -75,96 +115,69 @@ class DataImporter:
         shot_number: int = 0
         shots: list = []
 
-        CHUNKSIZE: Final = 10_000_000
-        TAIL_SIZE: Final = 1_000
+        for row_number, rawdata in enumerate(rawdata_df.itertuples()):
+            # ショット開始判定
+            if (not is_shot_section) and (rawdata.displacement <= start_displacement):
+                is_shot_section = True
+                is_target_of_cut_off = True
+                shot_number += 1
+                sequential_number_by_shot = 0
 
-        cols = ("load01", "load02", "load03", "load04", "displacement")
-        current_df_tail: DataFrame = pd.DataFrame(index=[], columns=cols)
+                # 荷重立ち上がり点取りこぼし防止
+                preceding_df: DataFrame = self._get_previous_df(row_number, rawdata_df, previous_df_tail, TAIL_SIZE)
 
-        dt_now = datetime.now()
+                for d in preceding_df.itertuples():
+                    shot = {
+                        "sequential_number": sequential_number,
+                        "sequential_number_by_shot": sequential_number_by_shot,
+                        "load01": d.load01,
+                        "load02": d.load02,
+                        "load03": d.load03,
+                        "load04": d.load04,
+                        "displacement": d.displacement,
+                        "shot_number": shot_number,
+                        "tags": [],
+                    }
+                    shots.append(shot)
+                    sequential_number += 1
+                    sequential_number_by_shot += 1
 
-        for loop_count, rawdata_df in enumerate(pd.read_csv(rawdata_filename, chunksize=CHUNKSIZE, names=cols)):
-            # スループット表示
-            if loop_count != 0:
-                processed_count: int = loop_count * CHUNKSIZE
-                throughput_counter(processed_count, dt_now)
+            # ショット区間の終了判定
+            MARGIN: Final = 0.1  # ノイズの影響等で変位値が単調減少しなかった場合、ショット区間がすぐに終わってしまうことを防ぐためのマージン
+            if is_shot_section and (rawdata.displacement > start_displacement + MARGIN):
+                is_shot_section = False
 
-            # chunk開始直後にショットを検知した場合、荷重開始点を含めるためにN件遡る。
-            # そのためのデータをprevious_df_tail（1つ前のchunkの末尾）とcurrent_df_tail（現在のchunkの末尾）に保持しておく。
-            previous_df_tail, current_df_tail = self._backup_df_tail(current_df_tail, rawdata_df, TAIL_SIZE)
+            # ショット未開始ならば後続は何もしない
+            if not is_shot_section:
+                continue
 
-            # chunk内のサンプルをイテレートしてショット切り出し
-            for row_number, rawdata in enumerate(rawdata_df.itertuples()):
-                # ショット開始判定
-                if (not is_shot_section) and (rawdata.displacement <= start_displacement):
-                    is_shot_section = True
-                    is_target_of_cut_off = True
-                    shot_number += 1
-                    sequential_number_by_shot = 0
+            # 切り出し区間の終了判定
+            if rawdata.displacement <= end_displacement:
+                is_target_of_cut_off = False
+                sequential_number_by_shot = 0
 
-                    preceding_df: DataFrame = self._get_previous_df(
-                        row_number, rawdata_df, previous_df_tail, TAIL_SIZE
-                    )
+            # 切り出し区間に到達していなければ後続は何もしない
+            if not is_target_of_cut_off:
+                continue
 
-                    for d in preceding_df.itertuples():
-                        shot = {
-                            "sequential_number": sequential_number,
-                            "sequential_number_by_shot": sequential_number_by_shot,
-                            "load01": d.load01,
-                            "load02": d.load02,
-                            "load03": d.load03,
-                            "load04": d.load04,
-                            "displacement": d.displacement,
-                            "shot_number": shot_number,
-                            "tags": [],
-                        }
-                        shots.append(shot)
-                        sequential_number += 1
-                        sequential_number_by_shot += 1
+            sequential_number += 1
+            sequential_number_by_shot += 1
 
-                # ショット区間の終了判定
-                MARGIN: Final = 0.1  # ノイズの影響等で変位値が単調減少しなかった場合、ショット区間がすぐに終わってしまうことを防ぐためのマージン
-                if is_shot_section and (rawdata.displacement > start_displacement + MARGIN):
-                    is_shot_section = False
+            # 切り出し対象としてリストに加える
+            shot = {
+                "sequential_number": sequential_number,
+                "sequential_number_by_shot": sequential_number_by_shot,
+                "load01": rawdata.load01,
+                "load02": rawdata.load02,
+                "load03": rawdata.load03,
+                "load04": rawdata.load04,
+                "displacement": rawdata.displacement,
+                "shot_number": shot_number,
+                "tags": [],
+            }
+            shots.append(shot)
 
-                # ショット未開始ならば後続は何もしない
-                if not is_shot_section:
-                    continue
-
-                # 切り出し区間の終了判定
-                if rawdata.displacement <= end_displacement:
-                    is_target_of_cut_off = False
-                    sequential_number_by_shot = 0
-
-                # 切り出し区間に到達していなければ後続は何もしない
-                if not is_target_of_cut_off:
-                    continue
-
-                sequential_number += 1
-                sequential_number_by_shot += 1
-
-                # 切り出し対象としてリストに加える
-                shot = {
-                    "sequential_number": sequential_number,
-                    "sequential_number_by_shot": sequential_number_by_shot,
-                    "load01": rawdata.load01,
-                    "load02": rawdata.load02,
-                    "load03": rawdata.load03,
-                    "load04": rawdata.load04,
-                    "displacement": rawdata.displacement,
-                    "shot_number": shot_number,
-                    "tags": [],
-                }
-                shots.append(shot)
-
-            # 1chunk分の処理が終わったら、検出したショットをElasticsearchに書き出し
-            if len(shots) > 0:
-                ElasticManager.multi_process_bulk(shots, shots_index, num_of_process, 5000)
-                shots = []
-
-        # end of all rawdata loop
-        dt_now = datetime.now()
-        logger.info("Cut_off finished.")
+        return shots
 
     def _backup_df_tail(self, current_df_tail: DataFrame, df: DataFrame, N: int) -> DataFrame:
         """ 1つ前のchunkの末尾を現在のchunkの末尾に更新し、現在のchunkの末尾を保持する """
