6826f678136b5f9720f02519a79306796d0bc8f0
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2020-12-18 08:19:35 +0000

    セッションは廃止し、events_indexで状態管理するよう変更。

--- data_collect_manager/views.py
+++ data_collect_manager/views.py
@@ -1,7 +1,8 @@
 import os
 import sys
 from flask import render_template, request, Response, session
-from datetime import datetime, timezone, timedelta
+from datetime import datetime
+from pytz import timezone
 import json
 
 from werkzeug import exceptions
@@ -13,9 +14,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../utils"))
 from elastic_manager import ElasticManager
 
 
-JST = timezone(timedelta(hours=+9), "JST")
-
-
 @app.route("/")
 def show_manager():
     """ TOP画面表示。configファイルのstatusによって画面切り替え """
@@ -25,7 +23,9 @@ def show_manager():
     cfm = ConfigFileManager()
 
     if not cfm.config_exists:
-        cfm.create({"status": "stop"})
+        successful: bool = cfm.create({"status": "stop"})
+        if not successful:
+            return Response(response=json.dumps({"successful": successful}), status=500)
         return render_template("manager.html", status="stop")
 
     # configファイルがある場合、直近のevents_indexから状態判定
@@ -35,16 +35,16 @@ def show_manager():
     if latest_event_index is None:
         app.logger.error("config file exists, but events_index not found.")
         # stop状態にして収集を止め、初期画面に戻す
-        session["events_index"] = ""
-        cfm.update({"status": "stop"})
-        return render_template("manager.html", status="stop")
+        successful: bool = cfm.update({"status": "stop"})
+        if not successful:
+            return Response(response=json.dumps({"successful": successful}), status=500)
 
-    session["events_index"] = latest_event_index
+        return render_template("manager.html", status="stop")
 
     # events_indexの最新documentから状態判定
     latest_events_index_doc: dict = ElasticManager.get_latest_events_index_doc(latest_event_index)
-
     event_type: str = latest_events_index_doc["event_type"]
+
     # pause状態の場合、pauseのままなのか再開されたかによって状態変更
     if event_type == "pause":
         if latest_events_index_doc.get("end_time") is not None:
@@ -58,25 +58,24 @@ def show_manager():
 def setup():
     """ 段取り開始(rawdata取得開始) """
 
-    now: datetime = datetime.now(JST)
+    # 基本的にUTCを使うが、events_index名のサフィックスだけはJSTを使う
+    utc_now: datetime = datetime.now(timezone("UTC"))
+    jst_now = utc_now.astimezone(timezone("Asia/Tokyo"))
 
     # events_index作成
-    events_index: str = "events-" + now.strftime("%Y%m%d%H%M%S")
+    events_index: str = "events-" + jst_now.strftime("%Y%m%d%H%M%S")
     ElasticManager.create_index(events_index)
 
-    session["events_index"] = events_index
-
     # events_indexに段取り開始(setup)を記録
-    session["event_id"] = 0
-    doc_id = session["event_id"]
-    query = {"event_id": session["event_id"], "event_type": "setup", "start_time": now}
+    doc_id = 0
+    query = {"event_id": doc_id, "event_type": "setup", "occurred_time": utc_now}
     successful: bool = ElasticManager.create_doc(events_index, doc_id, query)
 
     if not successful:
         return Response(response=json.dumps({"successful": successful}), status=500)
 
     # configファイル更新
-    start_time: str = now.strftime("%Y%m%d%H%M%S%f")
+    start_time: str = utc_now.strftime("%Y%m%d%H%M%S%f")
     params = {"status": "running", "start_time": start_time}
     cfm = ConfigFileManager()
     successful: bool = cfm.update(params, should_change_sequence=True)
@@ -91,18 +90,12 @@ def setup():
 def start():
     """ 開始(eventsに開始時間を記録) """
 
-    # configファイルを読み込み、events_index名を特定
-    # cfm = ConfigFileManager()
-    # config: dict = cfm.read_config()
-    # events_index: str = config["events_index"]
-
-    now: datetime = datetime.now()
+    utc_now: datetime = datetime.now(timezone("UTC"))
 
     # events_indexに開始イベントを記録
-    session["event_id"] += 1
-    doc_id = session["event_id"]
-    query = {"event_type": "start", "start_time": now}
-    events_index: str = session["events_index"]
+    events_index: str = ElasticManager.get_latest_events_index()
+    doc_id = ElasticManager.count(events_index)
+    query = {"event_id": doc_id, "event_type": "start", "occurred_time": utc_now}
     successful: bool = ElasticManager.create_doc(events_index, doc_id, query)
 
     if not successful:
@@ -115,18 +108,12 @@ def start():
 def pause():
     """ 中断(eventsに中断の開始時間を記録) """
 
-    # configファイルを読み込み、events_index名を特定
-    # cfm = ConfigFileManager()
-    # config: dict = cfm.read_config()
-    # events_index: str = config["events_index"]
-
-    now: datetime = datetime.now()
+    utc_now: datetime = datetime.now(timezone("UTC"))
 
     # 中断イベントの記録。中断終了時刻は現在時刻を仮置き。
-    session["event_id"] += 1
-    doc_id = session["event_id"]
-    query = {"event_type": "pause", "start_time": now, "end_time": now}
-    events_index: str = session["events_index"]
+    events_index: str = ElasticManager.get_latest_events_index()
+    doc_id = ElasticManager.count(events_index)
+    query = {"event_id": doc_id, "event_type": "pause", "start_time": utc_now}
     successful: bool = ElasticManager.create_doc(events_index, doc_id, query)
 
     if not successful:
@@ -139,17 +126,12 @@ def pause():
 def resume():
     """ 再開(eventsに再開した時間を記録) """
 
-    # configファイルを読み込み、events_index名を特定
-    # cfm = ConfigFileManager()
-    # config: dict = cfm.read_config()
-    # events_index: str = config["events_index"]
-
-    now: datetime = datetime.now()
+    utc_now: datetime = datetime.now(timezone("UTC"))
 
     # 再開時は中断時に記録されたイベントを更新
-    doc_id = session["event_id"]
-    query = {"end_time": now}
-    events_index: str = session["events_index"]
+    events_index: str = ElasticManager.get_latest_events_index()
+    doc_id = ElasticManager.count(events_index) - 1  # 更新対象は最新のdocument（pause）イベントである前提
+    query = {"end_time": utc_now}
     successful: bool = ElasticManager.update_doc(events_index, doc_id, query)
 
     if not successful:
@@ -162,10 +144,10 @@ def resume():
 def stop():
     """ 停止(rawdata取得終了) """
 
-    now: datetime = datetime.now()
+    utc_now: datetime = datetime.now(timezone("UTC"))
 
     # configファイル更新
-    end_time: str = now.strftime("%Y%m%d%H%M%S%f")
+    end_time: str = utc_now.strftime("%Y%m%d%H%M%S%f")
     params = {"status": "stop", "end_time": end_time}
     cfm = ConfigFileManager()
     successful: bool = cfm.update(params, should_change_sequence=True)
@@ -173,18 +155,11 @@ def stop():
     if not successful:
         return Response(response=json.dumps({"successful": successful}), status=500)
 
-    # config: dict = cfm.read_config()
-    # events_index: str = config["events_index"]
-
     # events_indexに停止イベントを記録
-    session["event_id"] += 1
-    doc_id = session["event_id"]
-    query = {"event_type": "stop", "end_time": now}
-    events_index = session["events_index"]
+    events_index: str = ElasticManager.get_latest_events_index()
+    doc_id = ElasticManager.count(events_index)
+    query = {"event_id": doc_id, "event_type": "stop", "occurred_time": utc_now}
     successful = ElasticManager.create_doc(events_index, doc_id, query)
-
-    session["event_id"] = 0
-    session["events_index"] = None
 
     if not successful:
         return Response(response=json.dumps({"successful": successful}), status=500)
@@ -202,17 +177,12 @@ def record_tag():
         app.logger.error(str(e))
         return Response(response=json.dumps({"successful": False, "message": str(e)}), status=500)
 
-    # cfm = ConfigFileManager()
-    # config: dict = cfm.read_config()
-    # events_index: str = config["events_index"]
-
-    now: datetime = datetime.now()
+    utc_now: datetime = datetime.now(timezone("UTC"))
 
     # events_indexに事象記録
-    session["event_id"] += 1
-    doc_id = session["event_id"]
-    query = {"event_type": "tag", "tag": tag, "start_time": now}
-    events_index = session["events_index"]
+    query = {"event_type": "tag", "tag": tag, "start_time": utc_now}
+    events_index: str = ElasticManager.get_latest_events_index()
+    doc_id = ElasticManager.count(events_index)
     successful: bool = ElasticManager.create_doc(events_index, doc_id, query)
 
     if not successful:
