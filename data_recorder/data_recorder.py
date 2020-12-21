b3c23c1177a58310eef3dc2a6a3744ef75d98ed2
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2020-12-21 04:42:24 +0000

    data_recorder実装中。ファイル名からの生成時刻特定やフィルター実装。

--- data_recorder/data_recorder.py
+++ data_recorder/data_recorder.py
@@ -1,12 +1,14 @@
-from datetime import datetime
+from datetime import datetime, time, timedelta
 import os
 import sys
 import json
 import glob
+import re
 import shutil
 import logging
 import logging.handlers
-from typing import Final
+from typing import Final, NamedTuple
+from collections import namedtuple
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../utils"))
 from elastic_manager import ElasticManager
@@ -25,14 +27,24 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+class FileInfo(NamedTuple):
+    file_path: str
+    timestamp: datetime
+
+
+# class Interval(NamedTuple):
+#     start_time: datetime
+#     end_time: datetime
+
+
 def record() -> None:
     """ """
     shared_dir = "data/pseudo_data/"
 
-    files: list = glob.glob(shared_dir + "*.dat")
-    if len(files) == 0:
+    file_list: list = glob.glob(shared_dir + "*.dat")
+    if len(file_list) == 0:
         return
-    files.sort()
+    file_list.sort()
 
     # configファイルからstart-endtimeを取得。
     settings_file_path = os.path.dirname(__file__) + "/../common/settings.json"
@@ -55,17 +67,47 @@ def record() -> None:
 
     logger.info(f"target section: {start_time} - {end_time}")
 
-    # 作成したファイルリストと、start-endtimeを比較し、start-endに含まれるファイルのみにフィルターする
+    # ファイルリストから時刻データを生成
+    files_timestamp = map(create_file_timestamp, file_list)
+    # [{"file_path": "xxx", "timestamp": "yyy"},...]
+    files_info: list = [FileInfo._make(row) for row in zip(file_list, files_timestamp)]
+
+    # 対象となるファイルに絞り込む
+    target_files: list = list(filter(lambda x: start_time <= x.timestamp <= end_time, files_info))
+    for x in target_files:
+        print(x)
+
     # 含まれないファイルは削除する
 
     # 区間に含まれるファイルがなければreturn
-    # あればそのstarttimeを名前とするディレクトリを作成し、そこにファイルを退避
+    if len(target_files) == 0:
+        logger.info(f"No files in target inteverl {start_time} - {end_time}.")
+        return
 
-    # 退避したファイルを1つずつ読み込み
-    # バイナリをデコードしてセンサーデータ読み取り
-    # 先頭行の時刻=ファイル名であるはずなので、その時刻を設定
-    # 行ごとにサンプリングレートから換算される時間を加算
-    # rawdata_indexおよびcsvに出力する
+    # starttimeを名前とするディレクトリを作成し、そこにファイルを退避
+    processed_dir_path = os.path.join(shared_dir, datetime.strftime(start_time, "%Y%m%d%H%M%S%f"))
+    os.makedirs(processed_dir_path, exist_ok=True)
+
+    for file in target_files:
+        # バイナリ読み込み
+
+        # csv, es出力
+
+        # 処理済みディレクトリに退避
+        # shutil.move(file, processed_dir_path)
+
+
+def create_file_timestamp(filename: str) -> dict:
+    """ ファイル名から日時データを作成する """
+
+    parts: list = re.findall(r"\d+", filename)
+    timestamp_str: str = parts[1] + parts[2] + parts[3]
+    timestamp: datetime = datetime.strptime(timestamp_str, "%Y%m%d%H%M%S%f")
+    return timestamp
+
+
+# def extract_files_in_target(file_info: dict, interval):
+#     if file_info.timestamp
 
 
 def main():
