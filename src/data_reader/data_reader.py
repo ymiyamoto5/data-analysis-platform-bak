361a20d7b4dd2d6bea65de28b630a6468452bd82
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-01-20 18:46:10 +0900

    num_of_proc 28

--- src/data_reader/data_reader.py
+++ src/data_reader/data_reader.py
@@ -7,13 +7,26 @@ import logging
 import logging.handlers
 import pandas as pd
 
-
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 from elastic_manager.elastic_manager import ElasticManager
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../utils"))
 from time_logger import time_log
 import common
+
+LOG_FILE: Final[str] = os.path.join(
+    common.get_config_value(common.APP_CONFIG_PATH, "log_dir"), "data_reader/data_reader.log"
+)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=common.MAX_LOG_SIZE, backupCount=common.BACKUP_COUNT),
+        logging.StreamHandler(),
+    ],
+)
+logger = logging.getLogger(__name__)
 
 
 class DataReader:
@@ -32,16 +45,18 @@ class DataReader:
     def read_all(self, index: str) -> DataFrame:
         """ 全件取得し、連番の昇順ソート結果を返す """
 
-        print("データを全件取得します。データ件数が多い場合、長時間かかる場合があります。")
+        logger.info("データを全件取得します。データ件数が多い場合、長時間かかる場合があります。")
 
         num_of_data: int = ElasticManager.count(index=index)
 
-        print(f"データ件数: {num_of_data}")
+        logger.info(f"データ件数: {num_of_data}")
 
         result: List[dict] = ElasticManager.multi_process_range_scan(
-            index=index, num_of_data=num_of_data, num_of_process=14
+            index=index, num_of_data=num_of_data, num_of_process=28
         )
         result.sort(key=lambda x: x["sequential_number"])
+
+        logger.info("Data reading has finished.")
 
         df: DataFrame = pd.DataFrame(result)
         return df
