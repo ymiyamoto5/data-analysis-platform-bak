e39b319fe7b5b02675acc0a8490f2fa3263f177c
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-05-21 16:35:02 +0900

    NUM_OF_PROCESSをcpu数に変更

--- src/utils/common.py
+++ src/utils/common.py
@@ -30,7 +30,7 @@ MARGIN: Final[int] = 0.1
 SAMPLING_RATE: Final[int] = 100_000
 SAMPLING_INTERVAL: Final[int] = 0.000010
 TIMESTAMP_MAX: Final[float] = datetime.max.replace(tzinfo=timezone("UTC")).timestamp()
-NUM_OF_PROCESS: Final[int] = 14
+NUM_OF_PROCESS: Final[int] = multiprocessing.cpu_count()
 NUM_OF_LOAD_SENSOR: Final[int] = 4
 LOG_LEVEL: List[str] = ["DEBUG", "INFO", "WARN", "ERROR", "CRITICAL"]
 DEFAULT_LOG_LEVEL: Final[str] = "INFO"
