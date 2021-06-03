9c3008147f8e782fc0be9d904b6d50964432a43f
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-06-03 18:00:49 +0900

    repush

---
+++ src/manage.py
@@ -0,0 +1,15 @@
+"""
+ ==================================
+  manage.py
+ ==================================
+
+  Copyright(c) 2021 UNIADEX, Ltd. All Rights Reserved.
+  CONFIDENTIAL
+  Author: UNIADEX, Ltd.
+
+"""
+
+from data_collect_manager import app
+
+if __name__ == "__main__":
+    app.run()
---
+++ src/utils/df_to_els.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from typing import List
+from pandas.core.frame import DataFrame
+import logging
+import logging.handlers
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+from elastic_manager.elastic_manager import ElasticManager
+
+logger = logging.getLogger(__name__)
+
+
+def df_to_els(df: DataFrame, index: str, mapping: str, setting: str):
+    """ DataFrameをList[dict]に変換し、指定したindex名でElasticsearchに登録する """
+
+    if ElasticManager.exists_index(index):
+        ElasticManager.delete_index(index)
+    ElasticManager.create_index(index=index, mapping_file=mapping, setting_file=setting)
+
+    data_list: List[dict] = df.to_dict(orient="records")
+
+    ElasticManager.bulk_insert(data_list, index)
+
+    logger.info(f"{index} created.")
