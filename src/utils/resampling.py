2fb8ec2bee36296f62093f62f154cc612cb36faa
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-06-17 11:13:27 +0900

    リサンプリング修正

--- src/utils/resampling.py
+++ src/utils/resampling.py
@@ -14,13 +14,15 @@ from df_to_els import df_to_els
 
 
 def resampling(shots_df: DataFrame, resampling_rate: int, exclude_shots: Tuple[int]):
-    """ """
+    """ 指定したrateでリサンプリング """
+
+    print("resampling start...")
 
     return shots_df[shots_df.sequential_number_by_shot % resampling_rate == 0].reset_index(drop=True)
 
 
 def convert_timestamp(df: DataFrame):
-    """ """
+    """ unixtimeをdatetimeに変換 """
 
     # def format_time(x: float) -> datetime:
     #     _x = round(x, 3)
@@ -48,9 +50,12 @@ if __name__ == "__main__":
     exclude_shots = (983, 1227, 1228, 1229, 1369, 1381)
 
     resample_df = resampling(shots_df, 100, exclude_shots)
-    converted_df = convert_timestamp(resample_df)
+    # converted_df = convert_timestamp(resample_df)
 
     mapping: str = common.get_config_value(common.APP_CONFIG_PATH, "mapping_resample_path")
     setting: str = common.get_config_value(common.APP_CONFIG_PATH, "setting_resample_path")
 
-    df_to_els(converted_df, resample_index, mapping, setting)
+    # df_to_els(converted_df, resample_index, mapping, setting)
+    df_to_els(resample_df, resample_index, mapping, setting)
+
+    print("resampling end")
