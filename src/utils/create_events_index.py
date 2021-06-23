0dbd6d7cfcd33f9a73037079a059ba62268a59a7
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-06-23 13:03:32 +0900

    create_event_index型エラー修正

--- src/utils/create_events_index.py
+++ src/utils/create_events_index.py
@@ -2,35 +2,23 @@ import os
 import sys
 import pandas as pd
 from datetime import datetime, timedelta
-from pandas.core.frame import DataFrame
-from typing import Final, Tuple
-from pytz import timezone
-
-sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
-from data_reader.data_reader import DataReader
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../utils"))
 import common
-from df_to_els import df_to_els
+from df_to_els import df_to_els  # type: ignore
 
 
-def create_events_index(start_datetime_jst: str) -> None:
+def create_events_index(start_datetime_jst: datetime) -> None:
 
-    start_datetime_jst_str = start_datetime_jst.isoformat()
-    index = "events-" + start_datetime_jst_str.replace("-", "").replace("T", "").replace(":", "")
+    start_datetime_jst_str: str = start_datetime_jst.isoformat()
+    index: str = "events-" + start_datetime_jst_str.replace("-", "").replace("T", "").replace(":", "")
 
-    start_datetime_utc = start_datetime_jst + timedelta(hours=-9)
-    start_datetime_utc_str = start_datetime_utc.isoformat()
+    start_datetime_utc: datetime = start_datetime_jst + timedelta(hours=-9)
+    start_datetime_utc_str: str = start_datetime_utc.isoformat()
 
     # 暫定でendは1時間後
     end_datetime_utc = start_datetime_utc + timedelta(hours=1)
     end_datetime_utc_str = end_datetime_utc.isoformat()
-
-    # start_time_str = t[:4] + "-" + t[4:6] + "-" + t[6:8] + "T" + t[8:10] + ":" + t[10:12] + ":" + t[12:14] + ".000000"
-    # # end_timeは暫定で1時間後とする
-    # end_time_str = (
-    #     t[:4] + "-" + t[4:6] + "-" + t[6:8] + "T" + str(int(t[8:10]) + 1) + ":" + t[10:12] + ":" + t[12:14] + ".000000"
-    # )
 
     events = [
         {"event_id": 0, "event_type": "setup", "occurred_time": start_datetime_utc_str},
