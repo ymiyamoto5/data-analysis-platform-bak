3d1b2c18c397c5f3b478da05a527c09a3187c2bb
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-08-03 16:48:06 +0900

    refs #73 Flask appからDBアクセスを可能にする
    * handler実装

--- src/data_collect_manager/models/gateway.py
+++ src/data_collect_manager/models/gateway.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from data_collect_manager.models.db import db
 from data_collect_manager.models.machine_gateway_mapping import MachineGatewayMapping
+from data_collect_manager.models.gateway_handler_mapping import GatewayHandlerMapping
+from data_collect_manager.models.handler import Handler
 
 
 @dataclass
@@ -14,6 +16,8 @@ class Gateway(db.Model):
     status: str
     log_level: int
 
+    handlers: Handler
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     gateway_name = db.Column(db.String(255), unique=True, nullable=False)
     sequence_number = db.Column(db.Integer)
@@ -22,3 +26,4 @@ class Gateway(db.Model):
     log_level = db.Column(db.Integer)
 
     machines = db.relationship("Machine", secondary=MachineGatewayMapping.__tablename__, back_populates="gateways")
+    handlers = db.relationship("Handler", secondary=GatewayHandlerMapping.__tablename__, back_populates="gateways")
--- src/utils/create_db.py
+++ src/utils/create_db.py
@@ -2,10 +2,10 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
-from data_collect_manager.models.machine import Machine
 from data_collect_manager.models.machine_type import MachineType
+from data_collect_manager.models.machine import Machine
 from data_collect_manager.models.gateway import Gateway
-from data_collect_manager.models.machine_gateway_mapping import MachineGatewayMapping
+from data_collect_manager.models.handler import Handler
 from data_collect_manager.models.db import register_db
 from flask import Flask
 
@@ -28,13 +28,44 @@ gateway_01 = Gateway(gateway_name="GW-01", sequence_number=1, gateway_result=0, 
 gateway_02 = Gateway(gateway_name="GW-02", sequence_number=1, gateway_result=0, status="running", log_level=5)
 gateway_03 = Gateway(gateway_name="GW-03", sequence_number=1, gateway_result=0, status="stop", log_level=5)
 
-machine_01.gateways.append(gateway_01)
-machine_02.gateways.append(gateway_02)
-machine_03.gateways.append(gateway_03)
+handler_01 = Handler(
+    handler_name="AD-USB-1608",
+    handler_type="USB_1608HS",
+    adc_serial_num="01F3D39C",
+    sampling_frequency=100000,
+    sampling_ch_num=5,
+    filewrite_time=1,
+)
+
+handler_02 = Handler(
+    handler_name="AD-USB-xxxx",
+    handler_type="USB_xxxxHS",
+    adc_serial_num="ZZZZZZZ",
+    sampling_frequency=1000,
+    sampling_ch_num=16,
+    filewrite_time=1,
+)
+
+handler_03 = Handler(
+    handler_name="AD-USB-yyyy",
+    handler_type="USB_yyyyHS",
+    adc_serial_num="YYYYYYYY",
+    sampling_frequency=1000,
+    sampling_ch_num=16,
+    filewrite_time=1,
+)
+
+gateway_01.handlers.append(handler_01)
+gateway_02.handlers.append(handler_02)
+gateway_03.handlers.append(handler_03)
 
 db.session.add(gateway_01)
 db.session.add(gateway_02)
 db.session.add(gateway_03)
+
+machine_01.gateways.append(gateway_01)
+machine_02.gateways.append(gateway_02)
+machine_03.gateways.append(gateway_03)
 
 db.session.add(machine_01)
 db.session.add(machine_02)
