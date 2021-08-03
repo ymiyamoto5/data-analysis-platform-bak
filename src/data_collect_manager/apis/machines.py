8e511f59cc2082a0f0e6dbc0ed96108f1286c582
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-08-03 11:02:02 +0900

    refs #73 Flask appからDBアクセスを可能にする
    * MachineType-Machineの実装
    * json response実装

--- src/data_collect_manager/apis/machines.py
+++ src/data_collect_manager/apis/machines.py
@@ -1,11 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from data_collect_manager.models.machine import Machine
+from data_collect_manager.models.machine_type import MachineType
+from data_collect_manager.models.db import db
 
 machines = Blueprint("machines", __name__)
 
 
 @machines.route("/machines", methods=["GET"])
 def get_machines():
+    """ MachineTypeを起点に関連エンティティを全結合したデータを返す。"""
 
-    machines = Machine().query.all()
-    return machines
+    # machines = db.session.query(Machine).join(MachineType)
+    machines = MachineType.query.all()
+
+    return jsonify(machines)
--- src/utils/create_db.py
+++ src/utils/create_db.py
@@ -3,18 +3,28 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 from data_collect_manager.models.machine import Machine
-from data_collect_manager.models.db import db, register_db
+from data_collect_manager.models.machine_type import MachineType
+from data_collect_manager.models.db import register_db
 from flask import Flask
 
 app = Flask("app")
 db = register_db(app)
+db.drop_all()
 db.create_all()
 
-machine_01 = Machine(id=1, machine_name="test_machine_01")
-machine_02 = Machine(id=2, machine_name="test_machine_02")
+machine_type_01 = MachineType(machine_type_name="プレス")
+machine_type_02 = MachineType(machine_type_name="圧力プレート")
+
+db.session.add(machine_type_01)
+db.session.add(machine_type_02)
+
+machine_01 = Machine(machine_name="テストプレス01", machine_type_id=1)
+machine_02 = Machine(machine_name="テストプレス02", machine_type_id=1)
+machine_03 = Machine(machine_name="テスト圧力プレート01", machine_type_id=2)
 
 db.session.add(machine_01)
 db.session.add(machine_02)
+db.session.add(machine_03)
 db.session.commit()
 
 for m in Machine.query.all():
