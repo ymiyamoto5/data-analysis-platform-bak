baa28091fd82cbbfb6c5d58b56247021ddef6366
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-08-06 19:03:49 +0900

    refs #36 プロジェクト構成変更
    * mypy.ini移動

--- backend/utils/df_to_els.py
+++ backend/utils/df_to_els.py
@@ -1,18 +1,15 @@
-import os
-import sys
 from typing import List
 from pandas.core.frame import DataFrame
 import logging
 import logging.handlers
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
-from elastic_manager.elastic_manager import ElasticManager
+from backend.elastic_manager.elastic_manager import ElasticManager
 
 logger = logging.getLogger(__name__)
 
 
 def df_to_els(df: DataFrame, index: str, mapping: str = None, setting: str = None):
-    """ DataFrameをList[dict]に変換し、指定したindex名でElasticsearchに登録する """
+    """DataFrameをList[dict]に変換し、指定したindex名でElasticsearchに登録する"""
 
     if ElasticManager.exists_index(index):
         ElasticManager.delete_index(index)
