ab3874e895061248e1fe3b4a27261ef2f6cde504
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-08-16 17:22:01 +0900

    refs #47 test_data_recorder修正
    * testオールパス

--- backend/tests/tests_data_recorder/conftest.py
+++ backend/tests/tests_data_recorder/conftest.py
@@ -13,6 +13,8 @@ import pytest
 import pathlib
 import dataclasses
 import struct
+from backend.data_collect_manager.models.handler import Handler
+from backend.common.dao import HandlerDAO
 
 
 @dataclasses.dataclass
@@ -29,11 +31,13 @@ class DatFiles:
 def dat_files(tmp_path):
     """datファイルのfixture"""
 
-    tmp_dat_1: pathlib.Path = tmp_path / "AD-00_20201216-080058.620753.dat"
-    tmp_dat_2: pathlib.Path = tmp_path / "AD-00_20201216-080059.620753.dat"
-    tmp_dat_3: pathlib.Path = tmp_path / "AD-00_20201216-080100.620753.dat"
-    tmp_dat_4: pathlib.Path = tmp_path / "AD-00_20201216-080101.620753.dat"
-    tmp_dat_5: pathlib.Path = tmp_path / "AD-00_20201216-080102.620753.dat"
+    machine_id: str = "machine-01"
+
+    tmp_dat_1: pathlib.Path = tmp_path / f"{machine_id}_AD-00_20201216-080058.620753.dat"
+    tmp_dat_2: pathlib.Path = tmp_path / f"{machine_id}_AD-00_20201216-080059.620753.dat"
+    tmp_dat_3: pathlib.Path = tmp_path / f"{machine_id}_AD-00_20201216-080100.620753.dat"
+    tmp_dat_4: pathlib.Path = tmp_path / f"{machine_id}_AD-00_20201216-080101.620753.dat"
+    tmp_dat_5: pathlib.Path = tmp_path / f"{machine_id}_AD-00_20201216-080102.620753.dat"
 
     binary = struct.pack("<ddddd", 10.0, 1.1, 2.2, 3.3, 4.4) + struct.pack("<ddddd", 9.0, 1.2, 2.3, 3.4, 4.5)
 
@@ -46,3 +50,15 @@ def dat_files(tmp_path):
     dat_files = DatFiles(tmp_path, tmp_dat_1, tmp_dat_2, tmp_dat_3, tmp_dat_4, tmp_dat_5)
 
     yield dat_files
+
+
+@pytest.fixture(autouse=True)
+def init_mock(mocker):
+    mocker.patch.object(
+        HandlerDAO,
+        "fetch_handler",
+        return_value=Handler(
+            sampling_ch_num=5,
+            sampling_frequency=100_000,
+        ),
+    )
