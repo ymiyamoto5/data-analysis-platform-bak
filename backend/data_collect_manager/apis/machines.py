8b2af5cfe32327af560da6ef87b276e2c930bc6b
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-08-23 10:37:41 +0900

    refs #57 DAO作成
    * machine用のDAO作成

--- backend/data_collect_manager/apis/machines.py
+++ backend/data_collect_manager/apis/machines.py
@@ -1,16 +1,11 @@
 from flask import Blueprint, jsonify, request
-from backend.data_collect_manager.models.machine import Machine
-from backend.data_collect_manager.models.gateway import Gateway
-from backend.data_collect_manager.models.handler import Handler
-from backend.data_collect_manager.models.sensor import Sensor
-from backend.data_collect_manager.models.db import db
+from backend.data_collect_manager.dao.machine_dao import MachineDAO
 from marshmallow import Schema, fields, ValidationError, validate
 import traceback
 from backend.data_collect_manager.apis.api_common import character_validate
 from backend.common.common_logger import logger
 from backend.common.error_message import ErrorMessage, ErrorTypes
 from backend.common import common
-from sqlalchemy.orm import joinedload
 
 machines = Blueprint("machines", __name__)
 
@@ -31,23 +26,18 @@ class MachineSchema(Schema):
             )
         )
     )
+    machine_type_id = fields.Int()
 
 
-machine_create_schema = MachineSchema(only=("machine_id", "machine_name"))
-machine_update_schema = MachineSchema(only=("machine_name", "collect_status"))
+machine_create_schema = MachineSchema(only=("machine_id", "machine_name", "machine_type_id"))
+machine_update_schema = MachineSchema(only=("machine_name", "collect_status", "machine_type_id"))
 
 
 @machines.route("/machines", methods=["GET"])
 def fetch_machines():
     """Machineを起点に関連エンティティを全結合したデータを返す"""
 
-    machines = Machine.query.options(
-        joinedload(Machine.machine_type),
-        joinedload(Machine.gateways)
-        .joinedload(Gateway.handlers)
-        .joinedload(Handler.sensors)
-        .joinedload(Sensor.sensor_type),
-    ).all()
+    machines = MachineDAO.select_all()
 
     return jsonify(machines)
 
@@ -56,13 +46,7 @@ def fetch_machines():
 def fetch_machine(machine_id):
     """指定machineの情報を取得"""
 
-    machine = Machine.query.options(
-        joinedload(Machine.machine_type),
-        joinedload(Machine.gateways)
-        .joinedload(Gateway.handlers)
-        .joinedload(Handler.sensors)
-        .joinedload(Sensor.sensor_type),
-    ).get(machine_id)
+    machine = MachineDAO.select_by_id(machine_id)
 
     return jsonify(machine)
 
@@ -85,18 +69,10 @@ def create():
         message: str = ErrorMessage.generate_message(ErrorTypes.VALID_ERROR, e.messages)
         return jsonify({"message": message}), 400
 
-    machine_id: str = data["machine_id"]
-    machine_name: str = data["machine_name"]
-
-    new_machine = Machine(
-        machine_id=machine_id,
-        machine_name=machine_name,
-        collect_status=common.COLLECT_STATUS.RECORDED.value,
-    )
-
     try:
-        db.session.add(new_machine)
-        db.session.commit()
+        MachineDAO.insert(
+            machine_id=data["machine_id"], machine_name=data["machine_name"], machine_type_id=data["machine_type_id"]
+        )
         return jsonify({}), 200
     except Exception as e:
         logger.error(traceback.format_exc())
@@ -115,25 +91,21 @@ def update(machine_id):
         message: str = ErrorMessage.generate_message(ErrorTypes.NO_INPUT_DATA)
 
     try:
-        data = machine_update_schema.load(json_data)
+        update_data = machine_update_schema.load(json_data)
     except ValidationError as e:
         logger.error(traceback.format_exc())
         message: str = ErrorMessage.generate_message(ErrorTypes.VALUE_ERROR, e.messages)
         return jsonify({"message": message}), 400
 
-    machine = Machine.query.get(machine_id)
+    machine = MachineDAO.select_by_id(machine_id)
 
     if machine is None:
         message: str = ErrorMessage.generate_message(ErrorTypes.NOT_EXISTS, machine_id)
         logger.error(message)
         return jsonify({"message": message}), 404
 
-    # 更新対象のプロパティをセット
-    for key, value in data.items():
-        setattr(machine, key, value)
-
     try:
-        db.session.commit()
+        MachineDAO.update(machine, update_data)
         return jsonify({}), 200
     except Exception as e:
         logger.error(traceback.format_exc())
@@ -145,11 +117,8 @@ def update(machine_id):
 def delete(machine_id):
     """Machineの削除"""
 
-    machine = Machine.query.get(machine_id)
-
     try:
-        db.session.delete(machine)
-        db.session.commit()
+        MachineDAO.delete(machine_id)
         return jsonify({}), 200
     except Exception as e:
         logger.error(traceback.format_exc())
