efd439a4c0cbe0b10c21360d792793a061633112
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-08-25 13:49:36 +0900

    refs #66 履歴画面エラー
    * 修正完了

--- backend/data_collect_manager/apis/data_collect_history.py
+++ backend/data_collect_manager/apis/data_collect_history.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import List
 from flask import Blueprint, jsonify
+from backend.data_collect_manager.models.data_collect_history import DataCollectHistory
 from backend.data_collect_manager.dao.data_collect_history_dao import DataCollectHistoryDAO
 
 data_collect_history = Blueprint("data_collect_history", __name__)
@@ -10,15 +11,6 @@ data_collect_history = Blueprint("data_collect_history", __name__)
 def fetch_data_collect_history():
     """データ収集履歴を返す"""
 
-    history = DataCollectHistoryDAO.select_all()
-
-    # 表示用に変換
-    def _convert_date_time(x):
-        x.started_at = datetime.strftime(x.started_at, "%Y-%m-%d %H:%M:%S.%f")
-        if x.ended_at is not None:
-            x.ended_at = datetime.strftime(x.ended_at, "%Y-%m-%d %H:%M:%S.%f")
-        return x
-
-    history = list(map(_convert_date_time, history))
+    history: List[DataCollectHistory] = DataCollectHistoryDAO.select_all()
 
     return jsonify(history)
