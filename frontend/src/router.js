492ad3b2a192bacdd2adbd1dd4187d350fd8f221
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-08-25 10:11:00 +0900

    refs #39 管理画面実装
    * router設定

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -7,6 +7,8 @@ export const DATA_COLLECT_NAME = 'data_collect'
 export const DATA_COLLECT_PATH = ROOT_PATH + DATA_COLLECT_NAME
 export const DATA_COLLECT_HISTORY_NAME = 'data_collect_history'
 export const DATA_COLLECT_HISTORY_PATH = ROOT_PATH + DATA_COLLECT_HISTORY_NAME
+export const CONFIGURE_NAME = 'configure'
+export const CONFIGURE_PATH = ROOT_PATH + CONFIGURE_NAME
 
 const routes = [
   // '/'へのアクセスは/data_collectにリダイレクト
@@ -23,6 +25,11 @@ const routes = [
     path: DATA_COLLECT_HISTORY_PATH,
     name: DATA_COLLECT_HISTORY_NAME,
     component: () => import('@/views/DataCollectHistory.vue'),
+  },
+  {
+    path: CONFIGURE_PATH,
+    name: CONFIGURE_NAME,
+    component: () => import('@/views/Configure.vue'),
   },
 ]
 
