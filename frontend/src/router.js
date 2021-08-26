83308d1b5f385066dd2a3a78a03b9c2d6ec6c625
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-08-26 15:53:12 +0900

    refs #39 管理画面実装
    * configureからsettingsに名前変更
    * MachineSettings.vueでデータ取得実装

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -7,19 +7,16 @@ export const DATA_COLLECT_NAME = 'data_collect'
 export const DATA_COLLECT_PATH = ROOT_PATH + DATA_COLLECT_NAME
 export const DATA_COLLECT_HISTORY_NAME = 'data_collect_history'
 export const DATA_COLLECT_HISTORY_PATH = ROOT_PATH + DATA_COLLECT_HISTORY_NAME
-export const CONFIGURE_NAME = 'configure'
-export const CONFIGURE_PATH = ROOT_PATH + CONFIGURE_NAME
+export const SETTINGS_NAME = 'settings'
+export const SETTINGS_PATH = ROOT_PATH + SETTINGS_NAME
 export const MACHINE_SETTINGS_NAME = 'machine'
-export const MACHINE_SETTINGS_PATH =
-  CONFIGURE_PATH + '/' + MACHINE_SETTINGS_NAME
+export const MACHINE_SETTINGS_PATH = SETTINGS_PATH + '/' + MACHINE_SETTINGS_NAME
 export const GATEWAY_SETTINGS_NAME = 'gateway'
-export const GATEWAY_SETTINGS_PATH =
-  CONFIGURE_PATH + '/' + GATEWAY_SETTINGS_NAME
+export const GATEWAY_SETTINGS_PATH = SETTINGS_PATH + '/' + GATEWAY_SETTINGS_NAME
 export const HANDLER_SETTINGS_NAME = 'handler'
-export const HANDLER_SETTINGS_PATH =
-  CONFIGURE_PATH + '/' + HANDLER_SETTINGS_NAME
+export const HANDLER_SETTINGS_PATH = SETTINGS_PATH + '/' + HANDLER_SETTINGS_NAME
 export const SENSOR_SETTINGS_NAME = 'sensor'
-export const SENSOR_SETTINGS_PATH = CONFIGURE_PATH + '/' + SENSOR_SETTINGS_NAME
+export const SENSOR_SETTINGS_PATH = SETTINGS_PATH + '/' + SENSOR_SETTINGS_NAME
 
 const routes = [
   // '/'へのアクセスは/data_collectにリダイレクト
@@ -38,9 +35,9 @@ const routes = [
     component: () => import('@/views/DataCollectHistory.vue'),
   },
   {
-    path: CONFIGURE_PATH,
-    name: CONFIGURE_NAME,
-    component: () => import('@/views/Configure.vue'),
+    path: SETTINGS_PATH,
+    name: SETTINGS_NAME,
+    component: () => import('@/views/Settings.vue'),
     children: [
       // default path
       {
--- frontend/src/views/MachineSettings.vue
+++ frontend/src/views/MachineSettings.vue
@@ -1,19 +1,18 @@
 <template>
   <v-data-table
     :headers="headers"
-    :items="desserts"
-    sort-by="calories"
+    :items="machines"
+    sort-by="machine_id"
     class="elevation-1"
   >
     <template v-slot:top>
       <v-toolbar flat>
         <v-toolbar-title>機器管理</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
         <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ on, attrs }">
             <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-              New Item
+              新規作成
             </v-btn>
           </template>
           <v-card>
@@ -21,50 +20,13 @@
               <span class="text-h5">{{ formTitle }}</span>
             </v-card-title>
 
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Dessert name"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.calories"
-                      label="Calories"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.fat"
-                      label="Fat (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.carbs"
-                      label="Carbs (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.protein"
-                      label="Protein (g)"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="blue darken-1" text @click="close">
-                Cancel
+                キャンセル
               </v-btn>
               <v-btn color="blue darken-1" text @click="save">
-                Save
+                保存
               </v-btn>
             </v-card-actions>
           </v-card>
@@ -72,12 +34,12 @@
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
             <v-card-title class="text-h5"
-              >Are you sure you want to delete this item?</v-card-title
+              >本当に削除してもよいですか？</v-card-title
             >
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="blue darken-1" text @click="closeDelete"
-                >Cancel</v-btn
+                >キャンセル</v-btn
               >
               <v-btn color="blue darken-1" text @click="deleteItemConfirm"
                 >OK</v-btn
@@ -96,53 +58,33 @@
         mdi-delete
       </v-icon>
     </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize">
-        Reset
-      </v-btn>
-    </template>
   </v-data-table>
 </template>
 
 <script>
+import { createBaseApiClient } from '@/api/apiBase'
+const MACHINES_API_URL = '/api/v1/machines'
+
 export default {
   data: () => ({
     dialog: false,
     dialogDelete: false,
+    editedIndex: -1,
     headers: [
       {
-        text: 'Dessert (100g serving)',
+        text: '機器ID',
         align: 'start',
-        sortable: false,
-        value: 'name',
+        value: 'machine_id',
       },
-      { text: 'Calories', value: 'calories' },
-      { text: 'Fat (g)', value: 'fat' },
-      { text: 'Carbs (g)', value: 'carbs' },
-      { text: 'Protein (g)', value: 'protein' },
-      { text: 'Actions', value: 'actions', sortable: false },
+      { text: '機器名', value: 'machine_name' },
+      { text: 'アクション', value: 'actions', sortable: false },
     ],
-    desserts: [],
-    editedIndex: -1,
-    editedItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    },
-    defaultItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    },
+    machines: [],
   }),
 
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      return this.editedIndex === -1 ? '新規作成' : '編集'
     },
   },
 
@@ -156,99 +98,42 @@ export default {
   },
 
   created() {
-    this.initialize()
+    this.fetchTableData()
   },
 
   methods: {
-    initialize() {
-      this.desserts = [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-        },
-      ]
+    fetchTableData: async function() {
+      const client = createBaseApiClient()
+      let data = []
+      await client
+        .get(MACHINES_API_URL)
+        .then((res) => {
+          if (res.data.length === 0) {
+            return
+          }
+          data = res.data
+          this.machines = data
+        })
+        .catch((e) => {
+          console.log(e.response.data.message)
+          this.confirm_dialog(e.response.data.message)
+        })
     },
 
     editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item)
+      this.editedIndex = this.machines.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialog = true
     },
 
     deleteItem(item) {
-      this.editedIndex = this.desserts.indexOf(item)
+      this.editedIndex = this.machines.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialogDelete = true
     },
 
     deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1)
+      this.machines.splice(this.editedIndex, 1)
       this.closeDelete()
     },
 
@@ -270,9 +155,9 @@ export default {
 
     save() {
       if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        Object.assign(this.machines[this.editedIndex], this.editedItem)
       } else {
-        this.desserts.push(this.editedItem)
+        this.machines.push(this.editedItem)
       }
       this.close()
     },
