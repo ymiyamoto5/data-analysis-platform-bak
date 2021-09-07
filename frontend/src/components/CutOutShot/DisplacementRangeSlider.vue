b63f013e319aafe75d079566b5b056622da0005b
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-09-07 16:39:28 +0900

    refs #91 ショット切り出し機能のWebAP取り込み検討
    * スライダー追加
    * layout調整

---
+++ frontend/src/components/CutOutShot/DisplacementRangeSlider.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-card flat color="transparent">
+    <v-card-text>
+      <v-range-slider
+        v-model="range"
+        :max="max"
+        :min="min"
+        hide-details
+        height="300px"
+        vertical
+        dense
+        step="0.1"
+        class="align-center"
+        @input="$emit('input', $event)"
+      >
+        <template v-slot:prepend>
+          <v-text-field
+            :value="range[0]"
+            class="mt-0 pt-0"
+            hide-details
+            single-line
+            type="number"
+            style="width: 60px"
+            @change="$set(range, 0, $event)"
+          ></v-text-field>
+        </template>
+        <template v-slot:append>
+          <v-text-field
+            :value="range[1]"
+            class="mt-0 pt-0"
+            hide-details
+            single-line
+            type="number"
+            style="width: 60px"
+            @change="$set(range, 1, $event)"
+          ></v-text-field>
+        </template>
+      </v-range-slider>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      min: 0,
+      max: 100,
+      range: [0, 100],
+    }
+  },
+  // computed: {
+  //   range: {
+  //     get() {
+  //       return this.innerRange
+  //     },
+  //     set(value) {
+  //       this.innerRange = value
+  //       this.$emit('setSliderValues', value)
+  //     },
+  //   },
+  // },
+}
+</script>
--- frontend/src/views/CutOutShot.vue
+++ frontend/src/views/CutOutShot.vue
@@ -1,37 +1,60 @@
 <template>
-  <div>
-    <MachineSelect @input="setMachine"></MachineSelect>
-    <CollectDataSelect
-      @input="setCollectData"
-      :value="machineId"
-    ></CollectDataSelect>
-    <v-row>
-      <v-col cols="12" sm="12" md="6" lg="4">
-        <ChartCard :machineId="machineId" :collectData="collectData" />
-      </v-col>
-    </v-row>
-    <!-- <range-slider @setSliderValues="setSliderValues"></range-slider> -->
-  </div>
+  <v-app>
+    <v-container>
+      <v-row dense>
+        <v-col cols="5">
+          <MachineSelect @input="setMachine"></MachineSelect>
+        </v-col>
+      </v-row>
+      <v-row dense>
+        <v-col cols="7">
+          <CollectDataSelect
+            @input="setCollectData"
+            :value="machineId"
+          ></CollectDataSelect>
+        </v-col>
+      </v-row>
+      <v-row dense>
+        <v-col cols="1">
+          <DisplacementRangeSlider
+            v-if="dataSelected"
+            @input="setDisplacementRange"
+          ></DisplacementRangeSlider>
+        </v-col>
+        <v-col cols="11">
+          <ChartCard :machineId="machineId" :collectData="collectData" />
+        </v-col>
+      </v-row>
+      <v-row justify="center" v-if="dataSelected">
+        <v-btn color="primary">ショット切り出し開始</v-btn>
+      </v-row>
+      <v-row justify="center" v-if="started">
+        <v-textarea></v-textarea>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-// import RangeSlider from "./range-slider.vue";
 import MachineSelect from '@/components/CutOutShot/MachineSelect.vue'
 import CollectDataSelect from '@/components/CutOutShot/CollectDataSelect.vue'
 import ChartCard from '@/components/CutOutShot/ChartCard.vue'
+import DisplacementRangeSlider from '@/components/CutOutShot/DisplacementRangeSlider.vue'
 
 export default {
   components: {
     MachineSelect,
-    // RangeSlider,
+    DisplacementRangeSlider,
     CollectDataSelect,
     ChartCard,
   },
   data() {
     return {
+      dataSelected: false,
+      started: false,
       machineId: '',
-      lower: 0,
-      upper: 0,
+      startDisplacement: 0,
+      endDisplacement: 0,
       collectData: '',
     }
   },
@@ -40,13 +63,16 @@ export default {
       this.machineId = value
     },
     setCollectData(value) {
-      console.log('set!!!', value)
       this.collectData = value
+      this.dataSelected = true
     },
-    // setSliderValues(range) {
-    //   this.lower = range[0];
-    //   this.upper = range[1];
-    // },
+    setDisplacementRange(range) {
+      console.log(range)
+      this.startDisplacement = range[1]
+      this.endDisplacement = range[0]
+    },
   },
 }
 </script>
+
+<style scoped></style>
