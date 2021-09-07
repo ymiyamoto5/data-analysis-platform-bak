d52c5af2025cfbae132eb689d0b84b329182ae64
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-09-07 18:28:51 +0900

    refs #91 ショット切り出し機能のWebAP取り込み検討
    * targetDirをCutOutShot.vueで取得するよう変更
    * cut_out_shot実行（別プロセス化は未実施）

--- frontend/src/views/CutOutShot.vue
+++ frontend/src/views/CutOutShot.vue
@@ -22,7 +22,7 @@
           ></DisplacementRangeSlider>
         </v-col>
         <v-col cols="11">
-          <ChartCard :machine_id="machine_id" :collectData="collectData" />
+          <ChartCard :machine_id="machine_id" :targetDir="targetDir" />
         </v-col>
       </v-row>
       <v-row justify="center" v-if="dataSelected">
@@ -33,10 +33,14 @@
 </template>
 
 <script>
+import { createBaseApiClient } from '@/api/apiBase'
 import MachineSelect from '@/components/CutOutShot/MachineSelect.vue'
 import CollectDataSelect from '@/components/CutOutShot/CollectDataSelect.vue'
 import ChartCard from '@/components/CutOutShot/ChartCard.vue'
 import DisplacementRangeSlider from '@/components/CutOutShot/DisplacementRangeSlider.vue'
+
+const TARGET_DIR_API_URL = '/api/v1/target_dir'
+const CUT_OUT_SHOT_API_URL = '/api/v1/cut_out_shot'
 
 export default {
   components: {
@@ -50,6 +54,7 @@ export default {
       dataSelected: false,
       started: false,
       machine_id: '',
+      targetDir: '',
       start_displacement: 0,
       end_displacement: 0,
       collectData: '',
@@ -62,14 +67,58 @@ export default {
     setCollectData(value) {
       this.collectData = value
       this.dataSelected = true
+      this.fetchTargetDir()
+    },
+    // 対象ディレクトリ名を取得
+    fetchTargetDir: async function() {
+      // ex) 2021/09/06 14:59:38 - 2021/09/06 15:00:00 => 2021/09/06 14:59:38 => UNIXTIME(ミリ秒)
+      const targetDate = Date.parse(this.collectData.split('-')[0].slice(0, -1))
+
+      const client = createBaseApiClient()
+      await client
+        .get(TARGET_DIR_API_URL, {
+          params: {
+            machine_id: this.machine_id,
+            targetDate: targetDate,
+          },
+        })
+        .then((res) => {
+          if (res.data === null) {
+            return
+          }
+          this.targetDir = res.data
+        })
+        .catch((e) => {
+          console.log(e.response.data.message)
+        })
     },
     setDisplacementRange(range) {
       console.log(range)
       this.start_displacement = range[1]
       this.end_displacement = range[0]
     },
+    // ショット切り出し開始
     start: async function() {
       this.started = true
+
+      const postData = {
+        machine_id: this.machine_id,
+        start_displacement: this.start_displacement,
+        end_displacement: this.end_displacement,
+        target_dir: this.targetDir,
+      }
+
+      const client = createBaseApiClient()
+      await client
+        .post(CUT_OUT_SHOT_API_URL, postData)
+        .then((res) => {
+          if (res.data.length === 0) {
+            return
+          }
+        })
+        .catch((e) => {
+          console.log(e.response.data.message)
+        })
     },
   },
 }
