64ee0857c55197e99bf5d4d3217c398c799784b0
rfujimoto3 <rei.fujimoto@uniadex.co.jp>
2021-09-10 13:53:26 +0900

    refs #108 DBデータ取得時のtry/except実装（machine_type）
    * machine_typeの全データ取得失敗時に500エラーを返すように修正
    * データ取得時のユニットテストを実装

--- backend/data_collect_manager/apis/machine_types.py
+++ backend/data_collect_manager/apis/machine_types.py
@@ -2,6 +2,9 @@ from flask import Blueprint, jsonify
 from marshmallow import Schema, fields, ValidationError, validate
 from backend.data_collect_manager.apis.api_common import character_validate
 from backend.data_collect_manager.dao.machine_type_dao import MachineTypeDAO
+import traceback
+from backend.common.common_logger import logger
+from backend.common.error_message import ErrorMessage, ErrorTypes
 
 machine_types = Blueprint("machine_types", __name__)
 
@@ -19,6 +22,10 @@ machine_type_update_schema = MachineTypeSchema()
 @machine_types.route("/machine_types", methods=["GET"])
 def fetch_machine_types():
 
-    machine_types = MachineTypeDAO.select_all()
-
-    return jsonify(machine_types), 200
+    try:
+        machine_types = MachineTypeDAO.select_all()
+        return jsonify(machine_types), 200
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        message: str = ErrorMessage.generate_message(ErrorTypes.READ_FAIL, str(e))
+        return jsonify({"message": message}), 500
