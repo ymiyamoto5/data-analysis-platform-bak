2f5497e05385c130211c8b5fd12ca9c94a3bb860
rfujimoto3 <rei.fujimoto@uniadex.co.jp>
2021-09-10 14:11:10 +0900

    refs #108 DBデータ取得時のtry/except実装（data_collect_history）
    * data_collect_historyの全データ取得失敗時に500エラーを返すように修正
    * データ取得時のユニットテストを実装

--- backend/data_collect_manager/apis/data_collect_history.py
+++ backend/data_collect_manager/apis/data_collect_history.py
@@ -2,7 +2,12 @@ from datetime import datetime
 from typing import List
 from flask import Blueprint, jsonify
 from backend.data_collect_manager.models.data_collect_history import DataCollectHistory
-from backend.data_collect_manager.dao.data_collect_history_dao import DataCollectHistoryDAO
+from backend.data_collect_manager.dao.data_collect_history_dao import (
+    DataCollectHistoryDAO,
+)
+import traceback
+from backend.common.common_logger import logger
+from backend.common.error_message import ErrorMessage, ErrorTypes
 
 data_collect_history = Blueprint("data_collect_history", __name__)
 
@@ -11,6 +16,10 @@ data_collect_history = Blueprint("data_collect_history", __name__)
 def fetch_data_collect_history():
     """データ収集履歴を返す"""
 
-    history: List[DataCollectHistory] = DataCollectHistoryDAO.select_all()
-
-    return jsonify(history)
+    try:
+        history: List[DataCollectHistory] = DataCollectHistoryDAO.select_all()
+        return jsonify(history), 200
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        message: str = ErrorMessage.generate_message(ErrorTypes.READ_FAIL, str(e))
+        return jsonify({"message": message}), 500
