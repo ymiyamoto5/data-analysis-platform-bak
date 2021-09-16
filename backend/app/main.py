b6f7ab920d0407ff7984e375b301a24eb9160dde
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-09-16 13:54:25 +0900

    refs #109 FastAPIへの移行検討
    * sampleモジュール削除

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.cors import CORSMiddleware
 from backend.app.api.api import api_router
