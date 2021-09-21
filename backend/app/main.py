6cbaad9a436504e97f1688ed9bcdcabb0abd376f
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-09-21 10:34:22 +0900

    * corsのoriginを*に変更 * fastapiのreloadを指定するとデータが読み込めないときがあるため廃止

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ templates = Jinja2Templates(directory="frontend/dist/")
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://localhost:3000", "http://localhost:8080"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
