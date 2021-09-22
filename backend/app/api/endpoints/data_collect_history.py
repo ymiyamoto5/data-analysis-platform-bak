46f4a3f50d4ed79b29f6b037e86c70c3b7bda168
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-09-22 11:45:33 +0900

    refs #95 データ収集履歴削除機能
    * 実装完了

--- backend/app/api/endpoints/data_collect_history.py
+++ backend/app/api/endpoints/data_collect_history.py
@@ -2,8 +2,9 @@ from typing import List
 from backend.app.models.data_collect_history import DataCollectHistory
 from backend.app.crud.crud_data_collect_history import CRUDDataCollectHistory
 from backend.app.schemas import data_collect_history
+from backend.app.services.data_collect_history_service import DataCollectHistoryService
 from backend.common import common
-from fastapi import Depends, APIRouter, Path
+from fastapi import Depends, APIRouter, Path, HTTPException
 from sqlalchemy.orm import Session
 from backend.app.api.deps import get_db
 
@@ -27,3 +28,30 @@ def fetch_data_collect_history_by_machine(
 
     history: List[DataCollectHistory] = CRUDDataCollectHistory.select_by_machine_id(db, machine_id)
     return history
+
+
+@router.delete("/{id}", response_model=data_collect_history.DataCollectHistory)
+def delete_data_collect_history(id: int, db: Session = Depends(get_db)):
+    """以下を削除する
+    * データ収集履歴レコード
+    * 削除対象の収集データディレクトリ
+    * 削除対象のElasticsearchインデックス
+    """
+
+    history: DataCollectHistory = CRUDDataCollectHistory.select_by_id(db, id)
+    if not history:
+        raise HTTPException(status_code=404, detail="対象の履歴が存在しません")
+
+    # 機器ID + 取得日時 文字列取得
+    target: str = DataCollectHistoryService.get_target(history)
+
+    # ディレクトリを削除
+    DataCollectHistoryService.delete_data_directory(target)
+
+    # Elasticsearchインデックスを削除
+    DataCollectHistoryService.delete_elastic_index(target)
+
+    # DBから履歴を削除
+    deleted: DataCollectHistory = CRUDDataCollectHistory.delete(db, db_obj=history)
+
+    return deleted
