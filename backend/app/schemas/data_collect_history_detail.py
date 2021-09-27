dbb571164cec7eba452d7c41431ed5588c19ca5a
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-09-27 17:44:55 +0900

    refs #135 data_collect_history_detail schema修正
    * 修正完了

--- backend/app/schemas/data_collect_history_detail.py
+++ backend/app/schemas/data_collect_history_detail.py
@@ -3,27 +3,19 @@ from typing import Optional
 
 
 class DataCollectHistoryDetailBase(BaseModel):
-    pass
-
-
-class DataCollectHistoryDetail(DataCollectHistoryDetailBase):
     data_collect_history_id: int
     sensor_id: str
     sensor_name: str = Field(..., max_length=255)
     sensor_type_id: str = Field(..., max_length=255)
-    base_volt: Optional[float] = None
-    base_load: Optional[float] = None
-    initial_volt: Optional[float] = None
+    base_volt: Optional[float] = Field(None, ge=0.0, le=100.0)
+    base_load: Optional[float] = Field(None, ge=0.0, le=100.0)
+    initial_volt: Optional[float] = Field(None, ge=0.0, le=100.0)
 
+
+class DataCollectHistoryDetail(DataCollectHistoryDetailBase):
     class Config:
         orm_mode = True
 
 
 class DataCollectHistoryDetailUpdate(DataCollectHistoryDetailBase):
-    data_collect_history_id: int
-    sensor_id: str
-    sensor_name: str = Field(..., max_length=255)
-    sensor_type_id: str = Field(..., max_length=255)
-    base_volt: Optional[float] = None
-    base_load: Optional[float] = None
-    initial_volt: Optional[float] = None
+    pass
