a01240acec4c2b621a0b2f1dd4e88ddfded84109
rfujimoto3 <rei.fujimoto@uniadex.co.jp>
2021-10-15 11:36:42 +0900

    refs #173 変位センサーであることを示すタプル定義
    * CUT_OUT_SHOT_SENSOR_TYPESをタプルにて定義
    * 該当箇所の置き換え

--- backend/app/api/endpoints/cut_out_shot.py
+++ backend/app/api/endpoints/cut_out_shot.py
@@ -4,7 +4,10 @@ from backend.app.api.deps import get_db
 from backend.app.crud.crud_data_collect_history import CRUDDataCollectHistory
 from backend.app.models.data_collect_history import DataCollectHistory
 from backend.app.models.data_collect_history_detail import DataCollectHistoryDetail
-from backend.app.schemas.cut_out_shot import CutOutShotPulse, CutOutShotStrokeDisplacement
+from backend.app.schemas.cut_out_shot import (
+    CutOutShotPulse,
+    CutOutShotStrokeDisplacement,
+)
 from backend.app.services.cut_out_shot_service import CutOutShotService
 from backend.common import common
 from backend.common.error_message import ErrorMessage, ErrorTypes
@@ -36,15 +39,17 @@ def fetch_cut_out_sensor(
     """切り出しの基準となるセンサーがストローク変位、パルスどちらであるかを返す"""
 
     # 機器に紐づく設定値を履歴から取得
-    history: DataCollectHistory = CRUDDataCollectHistory.select_by_machine_id_started_at(
-        db, machine_id, target_date_str
+    history: DataCollectHistory = (
+        CRUDDataCollectHistory.select_by_machine_id_started_at(
+            db, machine_id, target_date_str
+        )
     )
 
     # NOTE: 切り出しの基準となるセンサーはただひとつのみ存在する前提
     cut_out_sensor = [
         sensor
         for sensor in history.data_collect_history_details
-        if sensor.sensor_type_id in ("stroke_displacement", "pulse")
+        if sensor.sensor_type_id in common.CUT_OUT_SHOT_SENSOR_TYPES
     ][0]
 
     return {"cut_out_sensor": cut_out_sensor.sensor_type_id}
@@ -59,7 +64,9 @@ def fetch_shots(
 ):
     """対象区間の最初のpklファイルを読み込み、ストローク変位値をリサンプリングして返す"""
 
-    files_info: Optional[List[FileInfo]] = CutOutShotService.get_files_info(machine_id, target_date_str)
+    files_info: Optional[List[FileInfo]] = CutOutShotService.get_files_info(
+        machine_id, target_date_str
+    )
 
     if files_info is None:
         raise HTTPException(status_code=400, detail="対象ファイルがありません")
@@ -73,8 +80,10 @@ def fetch_shots(
     df: DataFrame = CutOutShotService.fetch_df(target_file)
 
     # 機器に紐づく設定値を履歴から取得
-    history: DataCollectHistory = CRUDDataCollectHistory.select_by_machine_id_started_at(
-        db, machine_id, target_date_str
+    history: DataCollectHistory = (
+        CRUDDataCollectHistory.select_by_machine_id_started_at(
+            db, machine_id, target_date_str
+        )
     )
 
     # DataCollectHistoryDetailはセンサー毎の設定値
@@ -85,17 +94,21 @@ def fetch_shots(
     cut_out_sensor_type: str = [
         sensor.sensor_type_id
         for sensor in history.data_collect_history_details
-        if sensor.sensor_type_id in ("stroke_displacement", "pulse")
+        if sensor.sensor_type_id in common.CUT_OUT_SHOT_SENSOR_TYPES
     ][0]
 
     # TODO: rate可変化
     rate: int = 1000 if cut_out_sensor_type == "stroke_displacement" else 10
     # rate: int = 1
 
-    resampled_df: DataFrame = CutOutShotService.resample_df(df, history.sampling_frequency, rate)
+    resampled_df: DataFrame = CutOutShotService.resample_df(
+        df, history.sampling_frequency, rate
+    )
 
     # センサー値を物理変換
-    converted_df: DataFrame = CutOutShotService.physical_convert_df(resampled_df, sensors)
+    converted_df: DataFrame = CutOutShotService.physical_convert_df(
+        resampled_df, sensors
+    )
 
     data: Dict[str, Any] = converted_df.to_dict(orient="records")
 
@@ -103,16 +116,22 @@ def fetch_shots(
 
 
 @router.post("/stroke_displacement")
-def cut_out_shot_stroke_displacement(cut_out_shot_in: CutOutShotStrokeDisplacement, db: Session = Depends(get_db)):
+def cut_out_shot_stroke_displacement(
+    cut_out_shot_in: CutOutShotStrokeDisplacement, db: Session = Depends(get_db)
+):
     """ストローク変位値でのショット切り出し"""
 
     try:
         # データ収集時の履歴から、収集当時の設定値を取得する
-        history: DataCollectHistory = CRUDDataCollectHistory.select_by_machine_id_started_at(
-            db, cut_out_shot_in.machine_id, cut_out_shot_in.target_date_str
+        history: DataCollectHistory = (
+            CRUDDataCollectHistory.select_by_machine_id_started_at(
+                db, cut_out_shot_in.machine_id, cut_out_shot_in.target_date_str
+            )
         )
     except Exception:
-        raise HTTPException(status_code=500, detail=ErrorMessage.generate_message(ErrorTypes.READ_FAIL))
+        raise HTTPException(
+            status_code=500, detail=ErrorMessage.generate_message(ErrorTypes.READ_FAIL)
+        )
 
     # TODO: margin動的設定
     cutter = StrokeDisplacementCutter(
@@ -141,13 +160,20 @@ def cut_out_shot_pulse(cut_out_shot_in: CutOutShotPulse, db: Session = Depends(g
 
     try:
         # データ収集時の履歴から、収集当時の設定値を取得する
-        history: DataCollectHistory = CRUDDataCollectHistory.select_by_machine_id_started_at(
-            db, cut_out_shot_in.machine_id, cut_out_shot_in.target_date_str
+        history: DataCollectHistory = (
+            CRUDDataCollectHistory.select_by_machine_id_started_at(
+                db, cut_out_shot_in.machine_id, cut_out_shot_in.target_date_str
+            )
         )
     except Exception:
-        raise HTTPException(status_code=500, detail=ErrorMessage.generate_message(ErrorTypes.READ_FAIL))
+        raise HTTPException(
+            status_code=500, detail=ErrorMessage.generate_message(ErrorTypes.READ_FAIL)
+        )
 
-    cutter = PulseCutter(threshold=cut_out_shot_in.threshold, sensors=history.data_collect_history_details)
+    cutter = PulseCutter(
+        threshold=cut_out_shot_in.threshold,
+        sensors=history.data_collect_history_details,
+    )
 
     # TODO: サブプロセスでcut_out_shot実行
     cut_out_shot = CutOutShot(
