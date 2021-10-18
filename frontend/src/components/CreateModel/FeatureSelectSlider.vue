53373273e0f7376e309cd478aa950a634bbf2348
rfujimoto3 <rei.fujimoto@uniadex.co.jp>
2021-10-18 15:16:15 +0900

    refs #171 特徴量選択をドロップダウンリストに変更
    * モデル管理画面の特徴量選択をスライダーからドロップダウンリストに修正

--- frontend/src/components/CreateModel/FeatureSelectSlider.vue
+++ frontend/src/components/CreateModel/FeatureSelectSlider.vue
@@ -1,37 +1,18 @@
 <template>
-    <v-card flat color="transparent">
-        <v-card-text>
-            <v-slider
-                label="特徴量"
-                v-model="value"
-                :max="max"
-                :min="min"
-                hide-details
-                dense
-                step="1"
-                thumb-label="always"
-                class="align-center"
-                @input="$emit('input', $event)"
-            >
-            </v-slider>
-        </v-card-text>
-    </v-card>
+  <container>
+    <v-select
+      class="select"
+      :items="features"
+      label="特徴量"
+      outlined
+      dense
+      @input="$emit('nof', $event)"
+    ></v-select>
+  </container>
 </template>
 
 <script>
 export default {
-    props: ['features'],
-    watch: {
-        features: function(new_features) {
-            this.max = new_features - 1
-        },
-    },
-    data() {
-        return {
-            min: 0,
-            max: 10,
-            value: 0,
-        }
-    },
+  props: ['features'],
 }
 </script>
