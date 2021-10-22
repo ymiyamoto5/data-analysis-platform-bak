2579477f1d7b00f30188239c3ca0edb4530e5455
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-10-22 17:14:39 +0900

    refs #186 タグ付け実装
    * frontendのイニシャル実装

---
+++ frontend/src/views/Tagging.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row dense>
+        <v-col cols="5">
+          <MachineSelect @input="setMachine"></MachineSelect>
+        </v-col>
+      </v-row>
+      <!-- <v-row dense>
+        <v-col cols="7">
+          <CollectDataSelect
+            @input="setCollectData"
+            :value="machineId"
+          ></CollectDataSelect>
+        </v-col>
+      </v-row> -->
+
+      <v-row justify="center" v-if="dataSelected">
+        <v-btn
+          color="primary"
+          @click="start"
+          :disabled="running"
+          :loading="running"
+          >ショット切り出し開始</v-btn
+        >
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+// import { createBaseApiClient } from '@/api/apiBase'
+import MachineSelect from '@/components/MachineSelect.vue'
+
+export default {
+  components: {
+    MachineSelect,
+  },
+  data() {
+    return {
+      dataSelected: false,
+      machineId: '',
+      targetDateStr: '', // yyyyMMdd文字列
+    }
+  },
+  methods: {
+    setMachine(value) {
+      this.targetDateStr = ''
+      this.machineId = value
+    },
+  },
+}
+</script>
+
+<style scoped></style>
