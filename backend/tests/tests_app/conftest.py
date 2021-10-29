90f1ef9cbe1dc4f4f9aceae0d58ff5b2e881d5a1
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-10-29 17:16:29 +0900

    refs #195 db関連のunittest追加
    * dbのfixtureを用意。Inmemoryだとtableが見つけられずエラーとなったため、物理ファイルを用意

--- backend/tests/tests_app/conftest.py
+++ backend/tests/tests_app/conftest.py
@@ -10,12 +10,43 @@
 """
 
 import pytest
+from backend.app.api.deps import get_db
+from backend.app.db.session import Base
 from backend.app.main import app
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import sessionmaker
+
+# INMEMORY_DB_URL = "sqlite:///:memory:"
+INMEMORY_DB_URL = "sqlite:////mnt/datadrive/temp.db"
 
 
 @pytest.fixture
 def client():
     client = TestClient(app)
+
+    engine = create_engine(INMEMORY_DB_URL, echo=True)
+
+    # NOTE: sqliteは既定で外部キー制約無効のため有効化する
+    def _fk_pragma_on_connect(dbapi_con, con_record):
+        dbapi_con.execute("pragma foreign_keys=ON")
+
+    event.listen(engine, "connect", _fk_pragma_on_connect)
+
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    # テスト用にオンメモリのSQLiteテーブルを初期化（関数ごとにリセット）
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    # DIを使ってFastAPIのDBの向き先をテスト用DBに変更
+    def get_test_db():
+        try:
+            db = SessionLocal()
+            yield db
+        finally:
+            db.close
+
+    app.dependency_overrides[get_db] = get_test_db
 
     yield client
