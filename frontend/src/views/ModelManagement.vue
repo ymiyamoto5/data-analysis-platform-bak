3596a65ac4d3fdec0c87cf9dc8bd3cbebd328a61
masaki.yamazaki@uniadex.co.jp <masaki.yamazaki@uniadex.co.jp>
2021-11-10 09:52:50 +0900

    refs #200 vueファイルのインデント修正

--- frontend/src/views/ModelManagement.vue
+++ frontend/src/views/ModelManagement.vue
@@ -1,27 +1,27 @@
 <template>
-    <v-app>
-        <v-tabs v-model="tab" align-with-title>
-            <v-tabs-slider></v-tabs-slider>
+  <v-app>
+    <v-tabs v-model="tab" align-with-title>
+      <v-tabs-slider></v-tabs-slider>
 
-            <v-tab v-for="item in items" :key="item">
-                {{ item }}
-            </v-tab>
-            <v-tab :href="mlflowUri" target="_blank">
-                MLflow
-            </v-tab>
-        </v-tabs>
-        <v-tabs-items v-model="tab">
-            <v-tab-item>
-                <CreateModel></CreateModel>
-            </v-tab-item>
-            <v-tab-item>
-                <ServingModel></ServingModel>
-            </v-tab-item>
-            <v-tab-item>
-                <PredictModel></PredictModel>
-            </v-tab-item>
-        </v-tabs-items>
-    </v-app>
+      <v-tab v-for="item in items" :key="item">
+        {{ item }}
+      </v-tab>
+      <v-tab :href="mlflowUri" target="_blank">
+        MLflow
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <CreateModel></CreateModel>
+      </v-tab-item>
+      <v-tab-item>
+        <ServingModel></ServingModel>
+      </v-tab-item>
+      <v-tab-item>
+        <PredictModel></PredictModel>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-app>
 </template>
 
 <script>
@@ -30,19 +30,19 @@ import ServingModel from '@/components/ServingModel.vue'
 import PredictModel from '@/components/PredictModel.vue'
 
 export default {
-    components: {
-        CreateModel,
-        ServingModel,
-        PredictModel,
-    },
-    data() {
-        return {
-            tab: null,
-            items: ['Create Model', 'Serving Model', 'Predict Model'],
-            mlflowUri: process.env.VUE_APP_MLFLOW_URL,
-        }
-    },
-    methods: {},
+  components: {
+    CreateModel,
+    ServingModel,
+    PredictModel,
+  },
+  data() {
+    return {
+      tab: null,
+      items: ['Create Model', 'Serving Model', 'Predict Model'],
+      mlflowUri: process.env.VUE_APP_MLFLOW_URL,
+    }
+  },
+  methods: {},
 }
 </script>
 
