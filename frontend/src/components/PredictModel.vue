903bfedb201f540cbde06b325ef9ffb217308696
masaki.yamazaki@uniadex.co.jp <masaki.yamazaki@uniadex.co.jp>
2021-11-10 09:52:50 +0900

    refs #200 vueファイルのインデント修正

--- frontend/src/components/PredictModel.vue
+++ frontend/src/components/PredictModel.vue
@@ -1,47 +1,42 @@
 <template>
-    <v-app>
-        <v-container class="mt-6 mb-6">
-            <v-row dense>
-                <v-col cols="5">
-                    <ModelSelect @selectInput="setModel"></ModelSelect>
-                </v-col>
-            </v-row>
-            <v-row dense>
-                <v-col cols="8">
-                    <DataSelect @input="setDataset"></DataSelect>
-                </v-col>
-            </v-row>
-            <v-row justify="center">
-                <v-btn
-                    color="primary"
-                    @click="predict"
-                    :disabled="running || !modelSelected || !dataSelected"
-                    :loading="running"
-                    >予測</v-btn
-                >
-            </v-row>
-        </v-container>
-        <v-snackbar
-            v-model="snackbar"
-            timeout="5000"
-            top
-            color="success"
-            style="white-space: pre;"
+  <v-app>
+    <v-container class="mt-6 mb-6">
+      <v-row dense>
+        <v-col cols="5">
+          <ModelSelect @selectInput="setModel"></ModelSelect>
+        </v-col>
+      </v-row>
+      <v-row dense>
+        <v-col cols="8">
+          <DataSelect @input="setDataset"></DataSelect>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-btn
+          color="primary"
+          @click="predict"
+          :disabled="running || !modelSelected || !dataSelected"
+          :loading="running"
+          >予測</v-btn
         >
-            {{ snackbarMessage }}
+      </v-row>
+    </v-container>
+    <v-snackbar
+      v-model="snackbar"
+      timeout="5000"
+      top
+      color="success"
+      style="white-space: pre;"
+    >
+      {{ snackbarMessage }}
 
-            <template v-slot:action="{ attrs }">
-                <v-btn
-                    color="white"
-                    text
-                    v-bind="attrs"
-                    @click="snackbar = false"
-                >
-                    Close
-                </v-btn>
-            </template>
-        </v-snackbar>
-    </v-app>
+      <template v-slot:action="{ attrs }">
+        <v-btn color="white" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </v-app>
 </template>
 
 <script>
@@ -52,62 +47,62 @@ import DataSelect from '@/components/PredictModel/DataSelect.vue'
 const PREDICT_API_URL = '/api/v1/models/predict'
 
 export default {
-    components: {
-        ModelSelect,
-        DataSelect,
+  components: {
+    ModelSelect,
+    DataSelect,
+  },
+  data() {
+    return {
+      running: false,
+      machineId: '',
+      targetDir: '',
+      model: '',
+      version: '',
+      modelSelected: false,
+      dataSelected: false,
+      data: [],
+      label: [],
+      snackbar: false,
+      snackbarMessage: '',
+    }
+  },
+  methods: {
+    setModel(value) {
+      this.model = value['model']
+      this.version = value['version']
+      this.modelSelected = !!this.version
     },
-    data() {
-        return {
-            running: false,
-            machineId: '',
-            targetDir: '',
-            model: '',
-            version: '',
-            modelSelected: false,
-            dataSelected: false,
-            data: [],
-            label: [],
-            snackbar: false,
-            snackbarMessage: '',
-        }
+    setDataset(value) {
+      this.machineId = value['machine']
+      this.targetDir = value['target']
+      this.shot = value['shot']
+      this.dataSelected = !!this.shot
     },
-    methods: {
-        setModel(value) {
-            this.model = value['model']
-            this.version = value['version']
-            this.modelSelected = !!this.version
-        },
-        setDataset(value) {
-            this.machineId = value['machine']
-            this.targetDir = value['target']
-            this.shot = value['shot']
-            this.dataSelected = !!this.shot
-        },
-        predict: async function() {
-            this.running = true
+    predict: async function() {
+      this.running = true
 
-            const postData = {
-                model: this.model,
-                version: this.version,
-                machine_id: this.machineId,
-                target_dir: this.targetDir,
-                shot: this.shot,
-            }
-            const client = createBaseApiClient()
-            await client
-                .post(PREDICT_API_URL, postData)
-                .then((res) => {
-                    this.running = false
-                    this.data = res.data.data
-                    this.label = res.data.label
-                    this.snackbarMessage = `予測が完了しました\n 予測結果:${this.label}`
-                    this.snackbar = true
-                })
-                .catch((e) => {
-                    console.log(e.response.data.detail)
-                })
-        },
+      const postData = {
+        model: this.model,
+        version: this.version,
+        machine_id: this.machineId,
+        target_dir: this.targetDir,
+        shot: this.shot,
+      }
+      const client = createBaseApiClient()
+      await client
+        .post(PREDICT_API_URL, postData)
+        .then((res) => {
+          this.running = false
+          this.data = res.data.data
+          this.label = res.data.label
+          this.snackbarMessage = `予測が完了しました\n 予測結果:${this.label}`
+          this.snackbar = true
+        })
+        .catch((e) => {
+          console.log(e.response.data.detail)
+        })
     },
+  },
 }
 </script>
 
--- frontend/src/components/PredictModel/DataSelect.vue
+++ frontend/src/components/PredictModel/DataSelect.vue
@@ -1,36 +1,36 @@
 <template>
-    <v-container>
-        <v-select
-            class="select"
-            :items="machines"
-            label="機器"
-            outlined
-            dense
-            @input="setMachine"
-        ></v-select>
-        <v-select
-            class="select"
-            :items="datasets"
-            label="収集データ"
-            outlined
-            dense
-            @input="setData"
-        ></v-select>
-        <v-select
-            class="select"
-            :items="shots"
-            label="ショット"
-            outlined
-            dense
-            @input="
-                $emit('input', {
-                    machine: machineId,
-                    target: selectedDataset,
-                    shot: $event,
-                })
-            "
-        ></v-select>
-    </v-container>
+  <v-container>
+    <v-select
+      class="select"
+      :items="machines"
+      label="機器"
+      outlined
+      dense
+      @input="setMachine"
+    ></v-select>
+    <v-select
+      class="select"
+      :items="datasets"
+      label="収集データ"
+      outlined
+      dense
+      @input="setData"
+    ></v-select>
+    <v-select
+      class="select"
+      :items="shots"
+      label="ショット"
+      outlined
+      dense
+      @input="
+        $emit('input', {
+          machine: machineId,
+          target: selectedDataset,
+          shot: $event,
+        })
+      "
+    ></v-select>
+  </v-container>
 </template>
 
 <script>
@@ -39,72 +39,72 @@ const DATA_LIST_API_URL = '/api/v1/features/raw/list'
 const DATA_LENGTH_API_URL = '/api/v1/features/raw/length'
 
 export default {
-    data() {
-        return {
-            machineId: '',
-            machines: [],
-            dataList: [],
-            datasets: [],
-            selectedDataset: '',
-            shots: [],
-        }
+  data() {
+    return {
+      machineId: '',
+      machines: [],
+      dataList: [],
+      datasets: [],
+      selectedDataset: '',
+      shots: [],
+    }
+  },
+  mounted() {
+    this.fetchDatas()
+  },
+  methods: {
+    fetchDatas: async function() {
+      const client = createBaseApiClient()
+      await client
+        .get(DATA_LIST_API_URL)
+        .then((res) => {
+          if (res.data.length === 0) {
+            return
+          }
+          this.dataList = res.data.data
+          this.machines = this.dataList.map((x) => x[0])
+        })
+        .catch((e) => {
+          console.log(e.response.data.detail)
+          this.errorDialog(e.response.data.detail)
+        })
     },
-    mounted() {
-        this.fetchDatas()
+    setMachine(value) {
+      this.machineId = value
+      this.datasets = this.dataList
+        .filter((x) => x[0] == value)
+        .map((x) => x[1])
     },
-    methods: {
-        fetchDatas: async function() {
-            const client = createBaseApiClient()
-            await client
-                .get(DATA_LIST_API_URL)
-                .then((res) => {
-                    if (res.data.length === 0) {
-                        return
-                    }
-                    this.dataList = res.data.data
-                    this.machines = this.dataList.map((x) => x[0])
-                })
-                .catch((e) => {
-                    console.log(e.response.data.detail)
-                    this.errorDialog(e.response.data.detail)
-                })
-        },
-        setMachine(value) {
-            this.machineId = value
-            this.datasets = this.dataList
-                .filter((x) => x[0] == value)
-                .map((x) => x[1])
-        },
-        setData(value) {
-            this.selectedDataset = value
-            this.fetchDataLength()
-        },
-        fetchDataLength: async function() {
-            const client = createBaseApiClient()
-            await client
-                .get(DATA_LENGTH_API_URL, {
-                    params: {
-                        machine_id: this.machineId,
-                        target_dir: this.selectedDataset,
-                    },
-                })
-                .then((res) => {
-                    if (res.data.length === 0) {
-                        return
-                    }
-                    this.shots = res.data.shots
-                })
-                .catch((e) => {
-                    console.log(e.response.data.detail)
-                    this.errorDialog(e.response.data.detail)
-                })
-        },
+    setData(value) {
+      this.selectedDataset = value
+      this.fetchDataLength()
     },
+    fetchDataLength: async function() {
+      const client = createBaseApiClient()
+      await client
+        .get(DATA_LENGTH_API_URL, {
+          params: {
+            machine_id: this.machineId,
+            target_dir: this.selectedDataset,
+          },
+        })
+        .then((res) => {
+          if (res.data.length === 0) {
+            return
+          }
+          this.shots = res.data.shots
+        })
+        .catch((e) => {
+          console.log(e.response.data.detail)
+          this.errorDialog(e.response.data.detail)
+        })
+    },
+  },
 }
 </script>
 
 <style scoped>
 .select {
-    width: 250px;
+  width: 250px;
 }
 </style>
