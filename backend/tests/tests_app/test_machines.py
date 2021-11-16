6c3c56610c882b66b4d90437f6e3553d6649dbb6
rfujimoto3 <rei.fujimoto@uniadex.co.jp>
2021-11-16 16:35:50 +0900

    refs #195 db関連のunittest追加
    * 異常系のテストメソッドを追記
    * 正常系のdeleteテストメソッド修正

--- backend/tests/tests_app/test_machines.py
+++ backend/tests/tests_app/test_machines.py
@@ -1,4 +1,5 @@
 import pytest
+from backend.app.crud.crud_machine import CRUDMachine
 
 
 class TestRead:
@@ -20,23 +21,48 @@ class TestRead:
 
         assert actual_code == 200
 
+    def test_db_select_all_failed(self, client, mocker, init):
+        mocker.patch.object(CRUDMachine, "select_all", side_effect=Exception("some exception"))
+        response = client.get(self.endpoint)
+
+        assert response.status_code == 500
+
+    def test_db_select_by_id_failed(self, client, mocker, init):
+        endpoint = f"{self.endpoint}/{self.machine_id}"
+        mocker.patch.object(CRUDMachine, "select_by_id", side_effect=Exception("some exception"))
+        response = client.get(endpoint)
+
+        assert response.status_code == 500
+
 
 class TestCreate:
     @pytest.fixture
     def init(self):
         # NOTE: 末尾スラッシュがないと307 redirectになってしまう。
         self.endpoint = "/api/v1/machines/"
+        self.data = {
+            "machine_id": "Test-Machine-001",
+            "machine_name": "Test-Press",
+            "machine_type_id": 1,
+        }
 
     def test_normal(self, client, init):
+
+        response = client.post(self.endpoint, json=self.data)
+
+        assert response.status_code == 200
+
+    def test_not_unique_machine_id(self, client, init):
+        """重複しているmachine_id"""
         data = {
-            "machine_id": "Test-Machine-001",
+            "machine_id": "test-machine-01",
             "machine_name": "Test-Press",
             "machine_type_id": 1,
         }
 
         response = client.post(self.endpoint, json=data)
 
-        assert response.status_code == 200
+        assert response.status_code == 400
 
     test_invalid_machine_id_data = [
         (
@@ -79,31 +105,65 @@ class TestCreate:
 
         assert actual_code == expected_code
 
+    def test_insert_failed(self, client, mocker, init):
+        mocker.patch.object(CRUDMachine, "insert", side_effect=Exception("some exception"))
+        response = client.post(self.endpoint, json=self.data)
+
+        assert response.status_code == 500
+
 
 class TestUpdate:
     @pytest.fixture
     def init(self):
         self.machine_id = "test-machine-01"
-        self.endpoint = "/api/v1/machines/" + self.machine_id
-
-    def test_normal(self, client, init):
-        data = {
+        self.endpoint = "/api/v1/machines"
+        self.data = {
             "machine_name": "test-update",
             "machine_type_id": 2,
         }
-        response = client.put(self.endpoint, json=data)
+
+    def test_normal(self, client, init):
+        endpoint = f"{self.endpoint}/{self.machine_id}"
+        response = client.put(endpoint, json=self.data)
 
         assert response.status_code == 200
+
+    def test_not_exist_machine_id(self, client, init):
+        """存在しないmachine_id"""
+        endpoint = f"{self.endpoint}/not-exist-machine-id"
+        response = client.put(endpoint, json=self.data)
+
+        assert response.status_code == 404
+
+    def test_update_failed(self, client, mocker, init):
+        endpoint = f"{self.endpoint}/{self.machine_id}"
+        mocker.patch.object(CRUDMachine, "update", side_effect=Exception("some exception"))
+        response = client.put(endpoint, json=self.data)
+
+        assert response.status_code == 500
 
 
 class TestDelete:
     @pytest.fixture
     def init(self):
-        self.machine_id = "test-machine-04"
-        self.endpoint = "/api/v1/machines/" + self.machine_id
+        self.endpoint = "/api/v1/machines"
 
     def test_normal(self, client, init):
-
-        response = client.delete(self.endpoint)
+        endpoint = f"{self.endpoint}/test-machine-04"
+        response = client.delete(endpoint)
 
         assert response.status_code == 200
+
+    def test_not_exist_machine_id(self, client, init):
+        """存在しないmachine_id"""
+        endpoint = f"{self.endpoint}/not-exist-machine-id"
+        response = client.delete(endpoint)
+
+        assert response.status_code == 404
+
+    def test_foreign_key_error(self, client, init):
+        """子が存在するmachine_id"""
+        endpoint = f"{self.endpoint}/test-machine-01"
+        response = client.delete(endpoint)
+
+        assert response.status_code == 500
