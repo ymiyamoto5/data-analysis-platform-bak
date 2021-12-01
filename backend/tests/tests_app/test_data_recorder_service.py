0a819bcbd46f7a4c30b52f9cfabccfae2a5666d4
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-12-01 18:06:28 +0900

    refs #244 data_recorderのWebAP取り込み検討
    * test_data_recorder_service修正

--- backend/tests/tests_app/test_data_recorder_service.py
+++ backend/tests/tests_app/test_data_recorder_service.py
@@ -1,62 +1,141 @@
-# """
-#  ==================================
-#   test_data_recorder.py
-#  ==================================
+"""
+ ==================================
+  test_data_recorder.py
+ ==================================
 
-#   Copyright(c) 2021 UNIADEX, Ltd. All Rights Reserved.
-#   CONFIDENTIAL
-#   Author: UNIADEX, Ltd.
+  Copyright(c) 2021 UNIADEX, Ltd. All Rights Reserved.
+  CONFIDENTIAL
+  Author: UNIADEX, Ltd.
 
-# """
+"""
 
-# from decimal import Decimal
+from datetime import datetime, timedelta
+from decimal import Decimal
 
-# from backend.app.services.data_recorder_service import DataRecorderService
-# from backend.file_manager.file_manager import FileManager
+from backend.app.models.data_collect_history import DataCollectHistory
+from backend.app.models.data_collect_history_detail import DataCollectHistoryDetail
+from backend.app.models.data_collect_history_event import DataCollectHistoryEvent
+from backend.app.models.machine import Machine
+from backend.app.services.data_recorder_service import DataRecorderService
+from backend.common import common
+from backend.file_manager.file_manager import FileManager
 
 
-# class TestReadBinaryFiles:
-#     def test_normal(self, dat_files):
-#         """正常系：バイナリファイルが正常に読めること"""
+class TestReadBinaryFiles:
+    def test_normal(self, dat_files):
+        """正常系：バイナリファイルが正常に読めること"""
 
-#         machine_id: str = "machine-01"
+        machine_id: str = "test-machine-01"
 
-#         file_infos = FileManager.create_files_info(dat_files.tmp_path._str, machine_id, "dat")
-#         file = file_infos[0]
+        file_infos = FileManager.create_files_info(dat_files.tmp_path._str, machine_id, "dat")
+        file = file_infos[0]
 
-#         actual = DataRecorderService.read_binary_files(
-#             file=file,
-#             sequential_number=0,
-#             timestamp=Decimal(file.timestamp),
-#             # latest_data_collect_history=
-#         )
-#         # latest_data_collect_history: DataCollectHistory,
-#         # displacement_sensor_id: str,
-#         # sensor_ids_other_than_displacement: List[str],
-#         # sampling_interval: Decimal,
-#         expected = (
-#             [
-#                 {
-#                     "sequential_number": 0,
-#                     "timestamp": Decimal(file.timestamp),
-#                     "stroke_displacement": 10.0,
-#                     "load01": 1.1,
-#                     "load02": 2.2,
-#                     "load03": 3.3,
-#                     "load04": 4.4,
-#                 },
-#                 {
-#                     "sequential_number": 1,
-#                     "timestamp": Decimal(file.timestamp) + Decimal(0.00001),
-#                     "stroke_displacement": 9.0,
-#                     "load01": 1.2,
-#                     "load02": 2.3,
-#                     "load03": 3.4,
-#                     "load04": 4.5,
-#                 },
-#             ],
-#             2,
-#             Decimal(file.timestamp) + Decimal(0.00001) + Decimal(0.00001),
-#         )
+        j_started_at_1 = datetime(2020, 8, 1, 9, 40, 30, 0)
 
-#         assert actual == expected
+        dummy_data_collect_history = DataCollectHistory(
+            machine_id="test-machine-01",
+            machine_name="デモ用プレス機",
+            machine_type_id=1,
+            started_at=j_started_at_1 + timedelta(hours=-9),
+            ended_at=j_started_at_1 + timedelta(hours=-9) + timedelta(hours=1),
+            sampling_frequency=100000,
+            sampling_ch_num=5,
+            processed_dir_path="/mnt/datadrive/data/xxx",
+            sample_count=0,
+            data_collect_history_events=[
+                DataCollectHistoryEvent(
+                    event_id=0,
+                    event_name=common.COLLECT_STATUS.SETUP.value,
+                    occurred_at=j_started_at_1 + timedelta(hours=-9),
+                ),
+                DataCollectHistoryEvent(
+                    event_id=1,
+                    event_name=common.COLLECT_STATUS.START.value,
+                    occurred_at=j_started_at_1 + timedelta(hours=-9),
+                ),
+                DataCollectHistoryEvent(
+                    event_id=2,
+                    event_name=common.COLLECT_STATUS.STOP.value,
+                    occurred_at=j_started_at_1 + timedelta(hours=-9) + timedelta(minutes=120),
+                ),
+                DataCollectHistoryEvent(
+                    event_id=3,
+                    event_name=common.COLLECT_STATUS.RECORDED.value,
+                    occurred_at=j_started_at_1 + timedelta(hours=-9) + timedelta(minutes=121),
+                ),
+            ],
+            data_collect_history_details=[
+                DataCollectHistoryDetail(
+                    sensor_id="stroke_displacement",
+                    sensor_name="ストローク変位",
+                    sensor_type_id="stroke_displacement",
+                    slope=1.0,
+                    intercept=0.0,
+                ),
+                DataCollectHistoryDetail(
+                    sensor_id="load01",
+                    sensor_name="load01",
+                    sensor_type_id="load",
+                    slope=1.0,
+                    intercept=0.0,
+                ),
+                DataCollectHistoryDetail(
+                    sensor_id="load02",
+                    sensor_name="load02",
+                    sensor_type_id="load",
+                    slope=1.0,
+                    intercept=0.0,
+                ),
+                DataCollectHistoryDetail(
+                    sensor_id="load03",
+                    sensor_name="load03",
+                    sensor_type_id="load",
+                    slope=1.0,
+                    intercept=0.0,
+                ),
+                DataCollectHistoryDetail(
+                    sensor_id="load04",
+                    sensor_name="load04",
+                    sensor_type_id="load",
+                    slope=1.0,
+                    intercept=0.0,
+                ),
+            ],
+        )
+
+        actual = DataRecorderService.read_binary_files(
+            file=file,
+            sequential_number=0,
+            timestamp=Decimal(file.timestamp),
+            data_collect_history=dummy_data_collect_history,
+            displacement_sensor_id="stroke_displacement",
+            sensor_ids_other_than_displacement=["load01", "load02", "load03", "load04"],
+            sampling_interval=Decimal(1.0 / dummy_data_collect_history.sampling_frequency),
+        )
+
+        expected = (
+            [
+                {
+                    "sequential_number": 0,
+                    "timestamp": Decimal(file.timestamp),
+                    "stroke_displacement": 10.0,
+                    "load01": 1.1,
+                    "load02": 2.2,
+                    "load03": 3.3,
+                    "load04": 4.4,
+                },
+                {
+                    "sequential_number": 1,
+                    "timestamp": Decimal(file.timestamp) + Decimal(0.00001),
+                    "stroke_displacement": 9.0,
+                    "load01": 1.2,
+                    "load02": 2.3,
+                    "load03": 3.4,
+                    "load04": 4.5,
+                },
+            ],
+            2,
+            Decimal(file.timestamp) + Decimal(0.00001) + Decimal(0.00001),
+        )
+
+        assert actual == expected
