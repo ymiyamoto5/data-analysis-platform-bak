f7ee8cf6b850274657bb271827cb717e9df701ef
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-12-09 18:56:16 +0900

    refs #244 data_recorderのWebAP取り込み検討
    * 不要箇所の削除

--- backend/app/worker/tasks/data_recorder.py
+++ backend/app/worker/tasks/data_recorder.py
@@ -7,10 +7,7 @@ from sqlalchemy.orm.session import Session
 
 @celery_app.task(acks_late=True)
 def data_recorder_task(machine_id: str) -> str:
-    # for i in range(1, 11):
-    #     sleep(1)
-    #     current_task.update_state(state="PROGRESS", meta={"process_percent": i * 10})
-    # return f"test task return {machine_id}, {processed_dir_path}"
+    """データ記録処理をceleryタスクに登録する"""
 
     current_task.update_state(state="PROGRESS", meta={"message": "data recording start"})
 
@@ -18,4 +15,4 @@ def data_recorder_task(machine_id: str) -> str:
 
     DataRecorderService.record(db, machine_id)
 
-    return f"task finished. {machine_id}"
+    return f"data recording task finished. {machine_id}"
