cd489fb4b27ca40070d76b3abb48762494c01b8a
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-12-13 18:11:30 +0900

    refs #251 ショット切り出しオンライン化
    * 処理対象ファイルリスト作成実装
    * テスト追加

--- backend/tests/tests_app/test_cut_out_shot_service.py
+++ backend/tests/tests_app/test_cut_out_shot_service.py
@@ -1,5 +1,9 @@
+from typing import List
+
 import pytest
 from backend.app.services.cut_out_shot_service import CutOutShotService
+from backend.common import common
+from backend.file_manager.file_manager import FileManager
 
 
 class TestAutoCutOutShot:
@@ -8,10 +12,39 @@ class TestAutoCutOutShot:
         self.machine_id = "test-machine-01"
 
     def test_no_files_in_dir(self, db, mocker, init):
-        mocker.patch.object(CutOutShotService, "get_files_info", return_value=[])
+        """データ収集完了済みで、pklファイルが存在しない場合"""
+
+        get_files_info_mock = mocker.patch.object(FileManager, "get_files", return_value=[])
+        get_collect_status_mock = mocker.patch.object(
+            CutOutShotService, "get_collect_status", return_value=common.COLLECT_STATUS.RECORDED.value
+        )
+
         CutOutShotService.auto_cut_out_shot(db, self.machine_id)
 
-        assert True
+        get_files_info_mock.assert_called_once()
+        get_collect_status_mock.assert_called_once_with(self.machine_id)
 
-    def test_no_target_files(self):
-        pass
+
+class TestGetTargetFiles:
+    def test_no_target_files(self, pkl_files):
+        """pklファイルが存在するがすでに処理済みの場合"""
+
+        all_files: List[str] = [
+            pkl_files.tmp_pkl_1._str,
+            pkl_files.tmp_pkl_2._str,
+            pkl_files.tmp_pkl_3._str,
+            pkl_files.tmp_pkl_4._str,
+            pkl_files.tmp_pkl_5._str,
+        ]
+
+        has_been_processed: List[str] = [
+            pkl_files.tmp_pkl_1._str,
+            pkl_files.tmp_pkl_2._str,
+            pkl_files.tmp_pkl_3._str,
+            pkl_files.tmp_pkl_4._str,
+            pkl_files.tmp_pkl_5._str,
+        ]
+
+        target_files = CutOutShotService.get_target_files(all_files, has_been_processed)
+
+        assert target_files == []
