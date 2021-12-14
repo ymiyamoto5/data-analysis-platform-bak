11c07b0f9d581d77a6b5484947a48533b617ba5a
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-12-14 14:49:48 +0900

    refs #251 ショット切り出しオンライン化
    * ユニットテスト追加

--- backend/tests/tests_cut_out_shot/conftest.py
+++ backend/tests/tests_cut_out_shot/conftest.py
@@ -9,6 +9,9 @@
 
 """
 
+import os
+import pathlib
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 from typing import List
 
@@ -34,7 +37,7 @@ def stroke_displacement_target():
 
     sensors = create_stroke_displacement_sensors()
     data_collect_history = create_data_collect_history()
-    cutter = StrokeDisplacementCutter(start_stroke_displacement=0, end_stroke_displacement=0, margin=0, sensors=sensors)  # dummy
+    cutter = StrokeDisplacementCutter(start_stroke_displacement=47.0, end_stroke_displacement=34.0, margin=0.1, sensors=sensors)  # dummy
 
     instance = CutOutShot(cutter=cutter, data_collect_history=data_collect_history, machine_id=machine_id, target=target_date_str)
 
@@ -480,3 +483,175 @@ def create_data_collect_history():
             ),
         ],
     )
+
+
+@dataclass
+class PklFiles:
+    tmp_path: pathlib.Path
+    tmp_pkl_1: pathlib.Path
+    tmp_pkl_2: pathlib.Path
+    # tmp_pkl_3: pathlib.Path
+    # tmp_pkl_4: pathlib.Path
+    # tmp_pkl_5: pathlib.Path
+
+
+@pytest.fixture
+def pkl_files(tmp_path):
+    """pklファイルのfixture"""
+
+    processed_dir: str = "20211111110000"
+    machine_id: str = "machine-01"
+    tmp_processed_dir = tmp_path / f"{processed_dir}"
+
+    os.mkdir(tmp_processed_dir)
+
+    tmp_pkl_1: pathlib.Path = tmp_processed_dir / f"{machine_id}_AD-00_20211111-110001.620753.pkl"
+    tmp_pkl_2: pathlib.Path = tmp_processed_dir / f"{machine_id}_AD-00_20211111-110002.620753.pkl"
+    # tmp_pkl_3: pathlib.Path = tmp_processed_dir / f"{machine_id}_AD-00_20211111-110003.620753.pkl"
+    # tmp_pkl_4: pathlib.Path = tmp_processed_dir / f"{machine_id}_AD-00_20211111-110004.620753.pkl"
+    # tmp_pkl_5: pathlib.Path = tmp_processed_dir / f"{machine_id}_AD-00_20211111-110005.620753.pkl"
+
+    rawdata_1 = [
+        # 切り出し区間前1
+        {
+            "sequential_number": 0,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 10, 111111).timestamp(),
+            "stroke_displacement": 49.284,
+            "load01": 0.223,
+            "load02": 0.211,
+            "load03": 0.200,
+            "load04": 0.218,
+        },
+        # 切り出し区間前2
+        {
+            "sequential_number": 1,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 11, 111111).timestamp(),
+            "stroke_displacement": 47.534,
+            "load01": 0.155,
+            "load02": 0.171,
+            "load03": 0.180,
+            "load04": 0.146,
+        },
+        # 切り出し区間1-1
+        {
+            "sequential_number": 2,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 12, 111111).timestamp(),
+            "stroke_displacement": 47.0,
+            "load01": 1.574,
+            "load02": 1.308,
+            "load03": 1.363,
+            "load04": 1.432,
+        },
+        # 切り出し区間1-2（margin=0.1により、すぐに切り出し区間が終了しないことの確認用データ）
+        {
+            "sequential_number": 3,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 13, 111111).timestamp(),
+            "stroke_displacement": 47.1,
+            "load01": 1.500,
+            "load02": 1.200,
+            "load03": 1.300,
+            "load04": 1.400,
+        },
+    ]
+    df_1 = pd.DataFrame(rawdata_1)
+    df_1.to_pickle(tmp_pkl_1)
+
+    rawdata_2 = [
+        # 切り出し区間1-3
+        {
+            "sequential_number": 4,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 14, 111111).timestamp(),
+            "stroke_displacement": 34.961,
+            "load01": -0.256,
+            "load02": -0.078,
+            "load03": 0.881,
+            "load04": 0.454,
+        },
+        # 切り出し区間後1
+        {
+            "sequential_number": 5,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 15, 111111).timestamp(),
+            "stroke_displacement": 30.599,
+            "load01": -0.130,
+            "load02": 0.020,
+            "load03": 0.483,
+            "load04": 0.419,
+        },
+        # 切り出し区間後2
+        {
+            "sequential_number": 6,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 16, 111111).timestamp(),
+            "stroke_displacement": 24.867,
+            "load01": -0.052,
+            "load02": 0.035,
+            "load03": 0.402,
+            "load04": 0.278,
+        },
+        # 切り出し区間後3(ストローク変位にmargin=0.1を加算した場合、ショットの終了と見做されないストローク変位値)
+        {
+            "sequential_number": 7,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 17, 111111).timestamp(),
+            "stroke_displacement": 47.100,
+            "load01": 0.155,
+            "load02": 0.171,
+            "load03": 0.180,
+            "load04": 0.146,
+        },
+        # 切り出し区間後4(ショット区間終了）
+        {
+            "sequential_number": 8,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 18, 111111).timestamp(),
+            "stroke_displacement": 47.150,
+            "load01": 0.156,
+            "load02": 0.172,
+            "load03": 0.181,
+            "load04": 0.147,
+        },
+        # 切り出し区間2-1
+        {
+            "sequential_number": 9,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 19, 111111).timestamp(),
+            "stroke_displacement": 47.0,
+            "load01": 1.574,
+            "load02": 1.308,
+            "load03": 1.363,
+            "load04": 1.432,
+        },
+        # 切り出し区間2-2（margin=0.1により、すぐに切り出し区間が終了しないことの確認用データ）
+        {
+            "sequential_number": 10,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 20, 111111).timestamp(),
+            "stroke_displacement": 47.1,
+            "load01": 1.500,
+            "load02": 1.200,
+            "load03": 1.300,
+            "load04": 1.400,
+        },
+        # 切り出し区間2-3
+        {
+            "sequential_number": 11,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 21, 111111).timestamp(),
+            "stroke_displacement": 34.961,
+            "load01": -0.256,
+            "load02": -0.078,
+            "load03": 0.881,
+            "load04": 0.454,
+        },
+        # 切り出し区間後
+        {
+            "sequential_number": 12,
+            "timestamp": datetime(2020, 12, 1, 10, 30, 22, 111111).timestamp(),
+            "stroke_displacement": 30.599,
+            "load01": -0.130,
+            "load02": 0.020,
+            "load03": 0.483,
+            "load04": 0.419,
+        },
+    ]
+
+    df_2 = pd.DataFrame(rawdata_2)
+    df_2.to_pickle(tmp_pkl_2)
+
+    pkl_files = PklFiles(tmp_processed_dir, tmp_pkl_1, tmp_pkl_2)
+
+    yield pkl_files
