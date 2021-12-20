d4d2be347c7ce47bae2ec57e958ae3eaae3d757d
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2021-12-20 15:22:39 +0900

    refs #271 データ収集履歴にDSLを持たせる
    * 修正完了

--- backend/app/schemas/data_collect_history_detail.py
+++ backend/app/schemas/data_collect_history_detail.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
@@ -8,6 +10,9 @@ class DataCollectHistoryDetailBase(BaseModel):
     sensor_type_id: str = Field(..., max_length=255)
     slope: float
     intercept: float
+    start_point_dsl: Optional[str] = None
+    max_point_dsl: Optional[str] = None
+    break_point_dsl: Optional[str] = None
 
 
 class DataCollectHistoryDetail(DataCollectHistoryDetailBase):
