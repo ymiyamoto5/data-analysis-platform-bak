b97c6743f6d3af19af9ee001d140e2bee4a164a5
rfujimoto3 <rei.fujimoto@uniadex.co.jp>
2022-01-05 16:11:38 +0900

    refs #268 CSVアップロード画面実装
    * FormDataにてファイルを含む必要なデータを渡すよう修正
    * 指定したディレクトリにアップロードする処理を実装
    * UI画面修正

--- backend/app/api/endpoints/csv_upload.py
+++ backend/app/api/endpoints/csv_upload.py
@@ -1,73 +1,31 @@
 import os
 import shutil
-from typing import Any, Optional
+from typing import List
 
 from backend.common import common
 from backend.common.common_logger import uvicorn_logger as logger
-from fastapi import APIRouter, File, Form, Path, UploadFile
-from pydantic import BaseModel, Field
+from fastapi import APIRouter, File, Form, UploadFile
 
 router = APIRouter()
 
 
-class Item(BaseModel):
-    file: UploadFile = File(...)
-    machine_id: str = Field(..., max_length=255, regex=common.ID_PATTERN)
-    datetime: str
-
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate_to_json
-
-    @classmethod
-    def validate_to_json(cls, value):
-        if isinstance(value, str):
-            return cls(**json.loads(value))
-        return value
-
-
-# class Item2(BaseModel):
-#     files: Dict
-#     # files: List
-#     machine_id: str = Field(..., max_length=255, regex=common.ID_PATTERN)
-#     datetime: str
-
-#     @classmethod
-#     def __get_validators__(cls):
-#         yield cls.validate_to_json
-
-#     @classmethod
-#     def validate_to_json(cls, value):
-#         if isinstance(value, str):
-#             return cls(**json.loads(value))
-#         return value
-
-
-class file(BaseModel):
-    name: str = Form(...)
-    lastModified: int = Form(...)
-    lastModifiedDate: Any = Form(...)
-    webkitRelativePath: Optional[str] = Form(...)
-    size: int = Form(...)
-
-
-@router.post("/{machine_id}-{datetime}")
-def upload(machine_id: str = Path(..., max_length=255, regex=common.ID_PATTERN), datetime: str = Path(...), file: UploadFile = File(...)):
+@router.post("/")
+def upload(
+    machine_id: str = Form(..., max_length=255, regex=common.ID_PATTERN), datetime: int = Form(...), files: List[UploadFile] = File(...)
+):
     """csvファイルのアップロード"""
 
+    dir_name: str = machine_id + "-" + str(datetime)
+    upload_dir_path: str = os.path.join(os.environ["data_dir"], dir_name)
+
     # アップロード用のディレクトリを作成
-    dir_name: str = machine_id + "-" + datetime
-
-    processed_dir_path: str = os.path.join(os.environ["data_dir"], dir_name)
-
-    if os.path.isdir(processed_dir_path):
-        logger.debug(f"{processed_dir_path} is already exists")
+    if os.path.isdir(upload_dir_path):
+        logger.debug(f"{upload_dir_path} is already exists")
     else:
-        os.makedirs(processed_dir_path)
-        logger.info(f"{processed_dir_path} created.")
+        os.makedirs(upload_dir_path)
+        logger.info(f"{upload_dir_path} created.")
 
-    # filesをアップロード実装中
-    # upload_path = os.environ["data_dir"] + dir_name
-    # for file in file:
-    with open(file.filename, "wb") as f:
-        shutil.copyfileobj(file.file, f)
+    # fileをアップロード
+    for file in files:
+        with open(os.path.join(upload_dir_path, file.filename), "wb+") as f:
+            shutil.copyfileobj(file.file, f)
