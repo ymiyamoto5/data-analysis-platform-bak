1e06873bfb9e97fc593ba9dc3fef86b451de2924
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2022-01-12 11:13:15 +0900

    Revert "refs #249 【オンラインショット】予測用モジュール「predictor」の追加"
    This reverts commit 18cb5861579c4303d6c7cb82ef8f5b5f979df124.

--- backend/app/worker/tasks/cut_out_shot.py
+++ backend/app/worker/tasks/cut_out_shot.py
@@ -10,7 +10,6 @@ from backend.app.db.session import SessionLocal
 from backend.app.models.data_collect_history import DataCollectHistory
 from backend.app.models.machine import Machine
 from backend.app.worker.celery import celery_app
-from backend.app.worker.tasks.common import get_collect_status
 from backend.common import common
 from backend.common.common_logger import logger
 from backend.cut_out_shot.cut_out_shot import CutOutShot
@@ -112,3 +111,15 @@ def get_target_files(all_files: List[str], has_been_processed: List[str]) -> Lis
             target_files.append(file)
 
     return target_files
+
+
+def get_collect_status(machine_id) -> str:
+    """データ収集ステータスを取得する"""
+
+    # NOTE: DBセッションを使いまわすと更新データが得られないため、新しいセッション作成
+    db: Session = SessionLocal()
+    machine: Machine = CRUDMachine.select_by_id(db, machine_id)
+    collect_status: str = machine.collect_status
+    db.close()
+
+    return collect_status
