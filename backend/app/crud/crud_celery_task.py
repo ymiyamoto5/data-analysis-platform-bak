cf97998c11e59e9cc71015a26f4a0ab06762b5aa
rfujimoto3 <rei.fujimoto@uniadex.co.jp>
2022-01-26 12:48:53 +0900

    refs #296 celeryタスク情報の取得方法検討・実装
    * .env.localの修正
    * タスク情報を取得するAPIを修正

--- backend/app/crud/crud_celery_task.py
+++ backend/app/crud/crud_celery_task.py
@@ -1,9 +1,25 @@
 from backend.app.models.celery_task import CeleryTask
 from backend.app.schemas import celery_task
+from celery.result import AsyncResult
 from sqlalchemy.orm import Session
 
 
 class CRUDCeleryTask:
+    @staticmethod
+    def select_by_id(task_id: str):
+        task = AsyncResult(task_id)
+
+        task_info = {
+            "task_id": task.task_id,
+            "status": task.status,
+            "result": task.result,
+            "traceback": task.traceback,
+            "children": task.children,
+            "date_done": task.date_done,
+        }
+
+        return task_info
+
     @staticmethod
     def insert(db: Session, obj_in: celery_task.CeleryTaskCreate) -> CeleryTask:
         new_data_celery_task = CeleryTask(
