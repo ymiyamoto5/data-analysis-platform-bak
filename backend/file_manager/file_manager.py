91684048539f623dac4c4cad202d6142dc094c7f
ymiyamoto5 <yuya.miyamoto@uniadex.co.jp>
2022-03-07 19:31:51 +0900

    refs #326 data_collect_history構造変更
    * dbのsampling_rateを100Hzに変更
    * file_managerのリトライロジック修正
    * gateway_simulatorでショット切り出しデバッグ実行可能な状態（画面からの実行は不可）

--- backend/file_manager/file_manager.py
+++ backend/file_manager/file_manager.py
@@ -3,6 +3,7 @@ import glob
 import os
 import re
 import sys
+import time
 from datetime import datetime
 from typing import Final, List
 
@@ -94,6 +95,7 @@ class FileManager:
         """
 
         MAX_RETRY_COUNT: Final[int] = 3
+        MAX_RETRY_INTERVAL: Final[int] = 3
         retry_count = 0
 
         while True:
@@ -110,16 +112,21 @@ class FileManager:
             pickle_files_by_handler: List[List[str]] = []  # [[ADC1_1, ADC1_2, ADC1_3, ...], [ADC2_1, ADC2_2, ADC2_3, ...], ...]
             for i, handler in enumerate(handlers):
                 pickle_files_in_handler: List[str] = [s for s in all_pickle_files if handler.handler_id in s]
-                pickle_files_by_handler.append(pickle_files_in_handler)
+                # 最初のハンドラーのファイル数を基準とする。
                 if i == 0:
                     tmp_len: int = len(pickle_files_in_handler)
+                # 2つめのハンドラー以降は、最初のハンドラーのファイル数と一致していること。一致しなければbreakして全pickleファイル再取得。
                 else:
                     if len(pickle_files_in_handler) != tmp_len:
                         retry_count += 1
                         break
+                pickle_files_by_handler.append(pickle_files_in_handler)
+
             if len(pickle_files_by_handler) == len(handlers):
                 logger.info("All handler has same number of files.")
                 break
+
+            time.sleep(MAX_RETRY_INTERVAL)
 
         file_set_list = []  # [[ADC1_1, ADC2_1, ADC3_1], [ADC1_2, ADC2_2, ADC3_2], ...]
         for file_number in range(len(pickle_files_by_handler[0])):
