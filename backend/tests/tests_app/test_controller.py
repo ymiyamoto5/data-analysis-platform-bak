ebad1078acc92841d819358c57b418a46b7adf8e
rfujimoto3 <rei.fujimoto@uniadex.co.jp>
2022-03-31 16:12:18 +0900

    refs #336 github actions CI
    * 余計なimport文を削除

--- backend/tests/tests_app/test_controller.py
+++ backend/tests/tests_app/test_controller.py
@@ -16,7 +16,6 @@ from backend.app.crud.crud_machine import CRUDMachine
 from backend.app.models.machine import Machine
 from backend.app.worker.celery import celery_app
 from backend.common import common
-from git import Object
 
 
 class TestSetup:
