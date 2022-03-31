fa97e131e36363ae82337d3be26eb09e347e37fd
rfujimoto3 <rei.fujimoto@uniadex.co.jp>
2022-03-31 09:30:19 +0900

    refs #336 github actions CI
    * test_appのコメントアウト解除

--- backend/tests/tests_app/test_data_collect_history.py
+++ backend/tests/tests_app/test_data_collect_history.py
@@ -1,129 +1,130 @@
-# import pytest
-# from backend.app.crud.crud_data_collect_history import CRUDDataCollectHistory
-# from backend.app.services.data_collect_history_service import DataCollectHistoryService
+import pytest
+from backend.app.crud.crud_data_collect_history import CRUDDataCollectHistory
+from backend.app.services.data_collect_history_service import \
+    DataCollectHistoryService
 
 
-# class TestRead:
-#     @pytest.fixture
-#     def init(self):
-#         self.endpoint = "/api/v1/data_collect_histories"
-#         self.machine_id = "test-machine-01"
-#         self.data_collect_history_id = 1
+class TestRead:
+    @pytest.fixture
+    def init(self):
+        self.endpoint = "/api/v1/data_collect_histories"
+        self.machine_id = "test-machine-01"
+        self.data_collect_history_id = 1
 
-#     def test_normal_db_select_all(self, client, init):
-#         response = client.get(self.endpoint)
-#         actual_code = response.status_code
+    def test_normal_db_select_all(self, client, init):
+        response = client.get(self.endpoint)
+        actual_code = response.status_code
 
-#         assert actual_code == 200
+        assert actual_code == 200
 
-#     def test_db_select_all_failed(self, client, mocker, init):
-#         mocker.patch.object(CRUDDataCollectHistory, "select_all", side_effect=Exception("some exception"))
-#         response = client.get(self.endpoint)
+    def test_db_select_all_failed(self, client, mocker, init):
+        mocker.patch.object(CRUDDataCollectHistory, "select_all", side_effect=Exception("some exception"))
+        response = client.get(self.endpoint)
 
-#         assert response.status_code == 500
+        assert response.status_code == 500
 
-#     def test_normal_db_select_by_machine_id(self, client, init):
-#         endpoint = f"{self.endpoint}/{self.machine_id}"
-#         response = client.get(endpoint)
-#         actual_code = response.status_code
+    def test_normal_db_select_by_machine_id(self, client, init):
+        endpoint = f"{self.endpoint}/{self.machine_id}"
+        response = client.get(endpoint)
+        actual_code = response.status_code
 
-#         assert actual_code == 200
+        assert actual_code == 200
 
-#     def test_db_select_by_machine_id_failed(self, client, mocker, init):
-#         endpoint = f"{self.endpoint}/{self.machine_id}"
-#         mocker.patch.object(CRUDDataCollectHistory, "select_by_machine_id", side_effect=Exception("some exception"))
-#         response = client.get(endpoint)
+    def test_db_select_by_machine_id_failed(self, client, mocker, init):
+        endpoint = f"{self.endpoint}/{self.machine_id}"
+        mocker.patch.object(CRUDDataCollectHistory, "select_by_machine_id", side_effect=Exception("some exception"))
+        response = client.get(endpoint)
 
-#         assert response.status_code == 500
+        assert response.status_code == 500
 
-#     def test_normal_db_select_latest_by_machine_id(self, client, init):
-#         endpoint = f"{self.endpoint}/{self.machine_id}/latest"
-#         response = client.get(endpoint)
-#         actual_code = response.status_code
+    def test_normal_db_select_latest_by_machine_id(self, client, init):
+        endpoint = f"{self.endpoint}/{self.machine_id}/latest"
+        response = client.get(endpoint)
+        actual_code = response.status_code
 
-#         assert actual_code == 200
+        assert actual_code == 200
 
-#     def test_db_select_latest_by_machine_id_failed(self, client, mocker, init):
-#         endpoint = f"{self.endpoint}/{self.machine_id}/latest"
-#         mocker.patch.object(CRUDDataCollectHistory, "select_latest_by_machine_id", side_effect=Exception("some exception"))
-#         response = client.get(endpoint)
+    def test_db_select_latest_by_machine_id_failed(self, client, mocker, init):
+        endpoint = f"{self.endpoint}/{self.machine_id}/latest"
+        mocker.patch.object(CRUDDataCollectHistory, "select_latest_by_machine_id", side_effect=Exception("some exception"))
+        response = client.get(endpoint)
 
-#         assert response.status_code == 500
-
-
-# class TestUpdate:
-#     @pytest.fixture
-#     def init(self):
-#         self.data_collect_history_id = 1
-#         self.endpoint = "/api/v1/data_collect_histories"
-#         self.data = {
-#             "sampling_frequency": 777,
-#             "data_collect_history_sensors": [
-#                 {
-#                     "data_collect_history_id": self.data_collect_history_id,
-#                     "sensor_id": "stroke_displacement",
-#                     "sensor_name": "ストローク変位",
-#                     "sensor_type_id": "stroke_displacement",
-#                     "slope": 7.0,
-#                     "intercept": 7.0,
-#                 },
-#                 {
-#                     "data_collect_history_id": self.data_collect_history_id,
-#                     "sensor_id": "load01",
-#                     "sensor_name": "荷重",
-#                     "sensor_type_id": "load01",
-#                     "slope": 7.0,
-#                     "intercept": 7.0,
-#                 },
-#             ],
-#         }
-
-#     def test_normal(self, client, init):
-#         endpoint = f"{self.endpoint}/{self.data_collect_history_id}"
-#         response = client.put(endpoint, json=self.data)
-
-#         assert response.status_code == 200
-
-#     def test_not_exist_data_collect_history_id(self, client, init):
-#         """存在しないdata_collect_history_id"""
-#         endpoint = f"{self.endpoint}/100"
-#         response = client.put(endpoint, json=self.data)
-
-#         assert response.status_code == 404
-
-#     def test_update_failed(self, client, mocker, init):
-#         endpoint = f"{self.endpoint}/{self.data_collect_history_id}"
-#         mocker.patch.object(CRUDDataCollectHistory, "update", side_effect=Exception("some exception"))
-#         response = client.put(endpoint, json=self.data)
-
-#         assert response.status_code == 500
+        assert response.status_code == 500
 
 
-# class TestDelete:
-#     @pytest.fixture
-#     def init(self):
-#         self.data_collect_history_id = 1
-#         self.endpoint = "/api/v1/data_collect_histories"
+class TestUpdate:
+    @pytest.fixture
+    def init(self):
+        self.data_collect_history_id = 1
+        self.endpoint = "/api/v1/data_collect_histories"
+        self.data = {
+            "sampling_frequency": 777,
+            "data_collect_history_sensors": [
+                {
+                    "data_collect_history_id": self.data_collect_history_id,
+                    "sensor_id": "stroke_displacement",
+                    "sensor_name": "ストローク変位",
+                    "sensor_type_id": "stroke_displacement",
+                    "slope": 7.0,
+                    "intercept": 7.0,
+                },
+                {
+                    "data_collect_history_id": self.data_collect_history_id,
+                    "sensor_id": "load01",
+                    "sensor_name": "荷重",
+                    "sensor_type_id": "load01",
+                    "slope": 7.0,
+                    "intercept": 7.0,
+                },
+            ],
+        }
 
-#     def test_normal(self, client, mocker, init):
-#         """子が存在するdata_collect_history_id"""
-#         mocker.patch.object(DataCollectHistoryService, "delete_elastic_index")
+    def test_normal(self, client, init):
+        endpoint = f"{self.endpoint}/{self.data_collect_history_id}"
+        response = client.put(endpoint, json=self.data)
 
-#         endpoint = f"{self.endpoint}/{self.data_collect_history_id}"
-#         response = client.delete(endpoint)
+        assert response.status_code == 200
 
-#         assert response.status_code == 200
+    def test_not_exist_data_collect_history_id(self, client, init):
+        """存在しないdata_collect_history_id"""
+        endpoint = f"{self.endpoint}/100"
+        response = client.put(endpoint, json=self.data)
 
-#     def test_not_exist_data_collect_history_id(self, client, init):
-#         """存在しないdata_collect_history_id"""
-#         endpoint = f"{self.endpoint}/100"
-#         response = client.delete(endpoint)
+        assert response.status_code == 404
 
-#         assert response.status_code == 404
+    def test_update_failed(self, client, mocker, init):
+        endpoint = f"{self.endpoint}/{self.data_collect_history_id}"
+        mocker.patch.object(CRUDDataCollectHistory, "update", side_effect=Exception("some exception"))
+        response = client.put(endpoint, json=self.data)
 
-#     def test_delete_failed(self, client, mocker, init):
-#         endpoint = f"{self.endpoint}/{self.data_collect_history_id}"
-#         mocker.patch.object(CRUDDataCollectHistory, "delete", side_effect=Exception("some exception"))
-#         response = client.delete(endpoint)
+        assert response.status_code == 500
 
-#         assert response.status_code == 500
+
+class TestDelete:
+    @pytest.fixture
+    def init(self):
+        self.data_collect_history_id = 1
+        self.endpoint = "/api/v1/data_collect_histories"
+
+    def test_normal(self, client, mocker, init):
+        """子が存在するdata_collect_history_id"""
+        mocker.patch.object(DataCollectHistoryService, "delete_elastic_index")
+
+        endpoint = f"{self.endpoint}/{self.data_collect_history_id}"
+        response = client.delete(endpoint)
+
+        assert response.status_code == 200
+
+    def test_not_exist_data_collect_history_id(self, client, init):
+        """存在しないdata_collect_history_id"""
+        endpoint = f"{self.endpoint}/100"
+        response = client.delete(endpoint)
+
+        assert response.status_code == 404
+
+    def test_delete_failed(self, client, mocker, init):
+        endpoint = f"{self.endpoint}/{self.data_collect_history_id}"
+        mocker.patch.object(CRUDDataCollectHistory, "delete", side_effect=Exception("some exception"))
+        response = client.delete(endpoint)
+
+        assert response.status_code == 500
--- backend/tests/tests_app/test_sensors.py
+++ backend/tests/tests_app/test_sensors.py
@@ -1,143 +1,143 @@
-# import pytest
-# from backend.app.crud.crud_sensor import CRUDSensor
+import pytest
+from backend.app.crud.crud_sensor import CRUDSensor
 
 
-# class TestRead:
-#     @pytest.fixture
-#     def init(self):
-#         self.endpoint = "/api/v1/sensors"
-#         self.sensor_id = "load01"
-#         self.machine_id = "test-machine-01"
+class TestRead:
+    @pytest.fixture
+    def init(self):
+        self.endpoint = "/api/v1/sensors"
+        self.sensor_id = "load01"
+        self.machine_id = "test-machine-01"
 
-#     def test_normal_db_select_all(self, client, init):
-#         response = client.get(self.endpoint)
-#         actual_code = response.status_code
+    def test_normal_db_select_all(self, client, init):
+        response = client.get(self.endpoint)
+        actual_code = response.status_code
 
-#         assert actual_code == 200
+        assert actual_code == 200
 
-#     def test_db_select_all_failed(self, client, mocker, init):
-#         mocker.patch.object(CRUDSensor, "select_all", side_effect=Exception("some exception"))
-#         response = client.get(self.endpoint)
+    def test_db_select_all_failed(self, client, mocker, init):
+        mocker.patch.object(CRUDSensor, "select_all", side_effect=Exception("some exception"))
+        response = client.get(self.endpoint)
 
-#         assert response.status_code == 500
+        assert response.status_code == 500
 
-#     def test_normal_db_select_by_id(self, client, init):
-#         endpoint = f"{self.endpoint}/{self.sensor_id}?machine_id={self.machine_id}"
-#         response = client.get(endpoint)
-#         actual_code = response.status_code
+    def test_normal_db_select_by_id(self, client, init):
+        endpoint = f"{self.endpoint}/{self.sensor_id}?machine_id={self.machine_id}"
+        response = client.get(endpoint)
+        actual_code = response.status_code
 
-#         assert actual_code == 200
+        assert actual_code == 200
 
-#     def test_db_select_by_id_failed(self, client, mocker, init):
-#         endpoint = f"{self.endpoint}/{self.sensor_id}?machine_id={self.machine_id}"
-#         mocker.patch.object(CRUDSensor, "select_by_id", side_effect=Exception("some exception"))
-#         response = client.get(endpoint)
+    def test_db_select_by_id_failed(self, client, mocker, init):
+        endpoint = f"{self.endpoint}/{self.sensor_id}?machine_id={self.machine_id}"
+        mocker.patch.object(CRUDSensor, "select_by_id", side_effect=Exception("some exception"))
+        response = client.get(endpoint)
 
-#         assert response.status_code == 500
-
-
-# class TestCreate:
-#     @pytest.fixture
-#     def init(self):
-#         # NOTE: 末尾スラッシュがないと307 redirectになってしまう。
-#         self.endpoint = "/api/v1/sensors/"
-#         self.data = {
-#             "sensor_name": "test-create",
-#             "sensor_type_id": "load",
-#             "handler_id": "test-handler-01-1",
-#             "slope": 1.0,
-#             "intercept": 0.0,
-#         }
-
-#     def test_normal(self, client, init):
-
-#         response = client.post(self.endpoint, json=self.data)
-
-#         assert response.status_code == 200
-
-#     def test_not_unique_sensor_id(self, client, init):
-#         """stroke_displacementを重複して登録
-#         NOTE: load等のセンサーはサフィックスをつけるため、基本的に重複しない。
-#         """
-#         data = {
-#             "sensor_name": "test-create",
-#             "sensor_type_id": "stroke_displacement",
-#             "handler_id": "test-handler-01-1",
-#             "slope": 1.0,
-#             "intercept": 0.0,
-#         }
-
-#         response = client.post(self.endpoint, json=data)
-
-#         assert response.status_code == 500
-
-#     def test_insert_failed(self, client, mocker, init):
-#         mocker.patch.object(CRUDSensor, "insert", side_effect=Exception("some exception"))
-#         response = client.post(self.endpoint, json=self.data)
-
-#         assert response.status_code == 500
+        assert response.status_code == 500
 
 
-# class TestUpdate:
-#     @pytest.fixture
-#     def init(self):
-#         self.sensor_id = "load01"
-#         self.endpoint = "/api/v1/sensors"
-#         self.data = {
-#             "machine_id": "test-machine-01",
-#             "sensor_name": "test-update",
-#             "sensor_type_id": "load",
-#             "handler_id": "test-handler-01-1",
-#             "slope": 10,
-#             "intercept": 10,
-#         }
+class TestCreate:
+    @pytest.fixture
+    def init(self):
+        # NOTE: 末尾スラッシュがないと307 redirectになってしまう。
+        self.endpoint = "/api/v1/sensors/"
+        self.data = {
+            "sensor_name": "test-create",
+            "sensor_type_id": "load",
+            "handler_id": "test-handler-01-1",
+            "slope": 1.0,
+            "intercept": 0.0,
+        }
 
-#     def test_normal(self, client, init):
-#         endpoint = f"{self.endpoint}/{self.sensor_id}"
-#         response = client.put(endpoint, json=self.data)
+    def test_normal(self, client, init):
 
-#         assert response.status_code == 200
+        response = client.post(self.endpoint, json=self.data)
 
-#     def test_not_exist_machine_id(self, client, init):
-#         """存在しないsensor_id"""
-#         endpoint = f"{self.endpoint}/not-exist-sensor-id"
-#         response = client.put(endpoint, json=self.data)
+        assert response.status_code == 200
 
-#         assert response.status_code == 404
+    def test_not_unique_sensor_id(self, client, init):
+        """stroke_displacementを重複して登録
+        NOTE: load等のセンサーはサフィックスをつけるため、基本的に重複しない。
+        """
+        data = {
+            "sensor_name": "test-create",
+            "sensor_type_id": "stroke_displacement",
+            "handler_id": "test-handler-01-1",
+            "slope": 1.0,
+            "intercept": 0.0,
+        }
 
-#     def test_update_failed(self, client, mocker, init):
-#         endpoint = f"{self.endpoint}/{self.sensor_id}"
-#         mocker.patch.object(CRUDSensor, "update", side_effect=Exception("some exception"))
-#         response = client.put(endpoint, json=self.data)
+        response = client.post(self.endpoint, json=data)
 
-#         assert response.status_code == 500
+        assert response.status_code == 500
+
+    def test_insert_failed(self, client, mocker, init):
+        mocker.patch.object(CRUDSensor, "insert", side_effect=Exception("some exception"))
+        response = client.post(self.endpoint, json=self.data)
+
+        assert response.status_code == 500
 
 
-# class TestDelete:
-#     @pytest.fixture
-#     def init(self):
-#         self.endpoint = "/api/v1/sensors"
-#         self.sensor_id = "load01"
-#         self.data = {
-#             "machine_id": "test-machine-01",
-#         }
+class TestUpdate:
+    @pytest.fixture
+    def init(self):
+        self.sensor_id = "load01"
+        self.endpoint = "/api/v1/sensors"
+        self.data = {
+            "machine_id": "test-machine-01",
+            "sensor_name": "test-update",
+            "sensor_type_id": "load",
+            "handler_id": "test-handler-01-1",
+            "slope": 10,
+            "intercept": 10,
+        }
 
-#     def test_normal(self, client, init):
-#         endpoint = f"{self.endpoint}/{self.sensor_id}"
-#         response = client.delete(endpoint, json=self.data)
+    def test_normal(self, client, init):
+        endpoint = f"{self.endpoint}/{self.sensor_id}"
+        response = client.put(endpoint, json=self.data)
 
-#         assert response.status_code == 200
+        assert response.status_code == 200
 
-#     def test_not_exist_sensor_id(self, client, init):
-#         """存在しないsensor_id"""
-#         endpoint = f"{self.endpoint}/not-exist-sensor-id"
-#         response = client.delete(endpoint, json=self.data)
+    def test_not_exist_machine_id(self, client, init):
+        """存在しないsensor_id"""
+        endpoint = f"{self.endpoint}/not-exist-sensor-id"
+        response = client.put(endpoint, json=self.data)
 
-#         assert response.status_code == 404
+        assert response.status_code == 404
 
-#     def test_delete_failed(self, client, mocker, init):
-#         endpoint = f"{self.endpoint}/{self.sensor_id}"
-#         mocker.patch.object(CRUDSensor, "delete", side_effect=Exception("some exception"))
-#         response = client.delete(endpoint, json=self.data)
+    def test_update_failed(self, client, mocker, init):
+        endpoint = f"{self.endpoint}/{self.sensor_id}"
+        mocker.patch.object(CRUDSensor, "update", side_effect=Exception("some exception"))
+        response = client.put(endpoint, json=self.data)
 
-#         assert response.status_code == 500
+        assert response.status_code == 500
+
+
+class TestDelete:
+    @pytest.fixture
+    def init(self):
+        self.endpoint = "/api/v1/sensors"
+        self.sensor_id = "load01"
+        self.data = {
+            "machine_id": "test-machine-01",
+        }
+
+    def test_normal(self, client, init):
+        endpoint = f"{self.endpoint}/{self.sensor_id}"
+        response = client.delete(endpoint, json=self.data)
+
+        assert response.status_code == 200
+
+    def test_not_exist_sensor_id(self, client, init):
+        """存在しないsensor_id"""
+        endpoint = f"{self.endpoint}/not-exist-sensor-id"
+        response = client.delete(endpoint, json=self.data)
+
+        assert response.status_code == 404
+
+    def test_delete_failed(self, client, mocker, init):
+        endpoint = f"{self.endpoint}/{self.sensor_id}"
+        mocker.patch.object(CRUDSensor, "delete", side_effect=Exception("some exception"))
+        response = client.delete(endpoint, json=self.data)
+
+        assert response.status_code == 500
