c2de02c72e0d70ac713d84713278e4aa1c938f7d
rfujimoto3 <rei.fujimoto@uniadex.co.jp>
2022-07-20 09:50:44 +0900

    refs #439 特徴抽出画面拡充
    * 各演算処理を実装
    * 回帰直線のグラフ描写については未対応

--- backend/dash_app/preprocess_view.py
+++ backend/dash_app/preprocess_view.py
@@ -5,7 +5,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from backend.dash_app.constants import CONTENT_STYLE, MAX_COLS, MAX_ROWS, PREPROCESS, SIDEBAR_STYLE
-from backend.dash_app.preprocessors import add, diff
+from backend.dash_app.preprocessors import add, calibration, diff, moving_average, mul, regression_line, shift, sub, thinning_out
 from backend.elastic_manager.elastic_manager import ElasticManager
 from dash import Dash, Input, Output, State, dash_table, dcc, html
 from plotly.subplots import make_subplots
@@ -15,13 +15,15 @@ app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 shot_df = pd.DataFrame(
     OrderedDict(
         [
-            ("sequential_number", [0, 1, 2, 3, 4, 5]),
+            # ("sequential_number", [0, 1, 2, 3, 4, 5]),
             ("shot_number", [1, 1, 1, 2, 2, 2]),
             ("load01", [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]),
             ("load02", [0.3, 0.5, 0.2, 0.1, 0.4, 0.8]),
         ]
     )
 )
+
+shot_df["sequential_number"] = shot_df.index
 
 table_df = pd.DataFrame()
 
@@ -39,6 +41,13 @@ def get_preprocess_dropdown_options():
         {"label": "", "value": ""},
         {"label": "微分", "value": PREPROCESS.DIFF.name},
         {"label": "加算", "value": PREPROCESS.ADD.name},
+        {"label": "減算", "value": PREPROCESS.SUB.name},
+        {"label": "係数乗算", "value": PREPROCESS.MUL.name},
+        {"label": "シフト", "value": PREPROCESS.SHIFT.name},
+        {"label": "校正", "value": PREPROCESS.CALIBRATION.name},
+        {"label": "移動平均", "value": PREPROCESS.MOVING_AVERAGE.name},
+        {"label": "回帰直線", "value": PREPROCESS.REGRESSION_LINE.name},
+        {"label": "間引き", "value": PREPROCESS.THINNING_OUT.name},
     ]
 
 
@@ -91,6 +100,62 @@ def serve_layout():
                         ],
                         style={"display": "none"},
                     ),
+                    html.Div(
+                        id="sub-field",
+                        children=[
+                            html.Label("減算列"),
+                            dcc.Dropdown(id="sub-field-dropdown", options=get_field_dropdown_options()),
+                        ],
+                        style={"display": "none"},
+                    ),
+                    html.Div(
+                        id="mul-field",
+                        children=[
+                            html.Label("係数", style={"width": "100%"}),
+                            dcc.Input(id="mul-field-dropdown", type="number", min=1, max=1000, step=1),
+                        ],
+                        style={"display": "none"},
+                    ),
+                    html.Div(
+                        id="shift-field",
+                        children=[
+                            html.Label("シフト幅", style={"width": "100%"}),
+                            dcc.Input(id="shift-field-dropdown", type="number", min=-1000, max=1000, step=1),
+                        ],
+                        style={"display": "none"},
+                    ),
+                    html.Div(
+                        id="calibration-field",
+                        children=[
+                            html.Label("先頭N件", style={"width": "100%"}),
+                            dcc.Input(id="calibration-field-dropdown", type="number", min=1, max=1000, step=1),
+                        ],
+                        style={"display": "none"},
+                    ),
+                    html.Div(
+                        id="moving-average-field",
+                        children=[
+                            html.Label("ウィンドウサイズ", style={"width": "100%"}),
+                            dcc.Input(id="moving-average-field-dropdown", type="number", min=1, max=100, step=1),
+                        ],
+                        style={"display": "none"},
+                    ),
+                    html.Div(
+                        id="regression-line-field",
+                        children=[
+                            html.Label("フィールド"),
+                            dcc.Dropdown(id="regression-line-field-dropdown", options=get_field_dropdown_options()),
+                        ],
+                        style={"display": "none"},
+                    ),
+                    html.Div(
+                        id="thinning-out-field",
+                        children=[
+                            html.Label("間引き幅", style={"width": "100%"}),
+                            dcc.Input(id="thinning-out-field-dropdown", type="number", min=1, max=1000, step=1),
+                        ],
+                        style={"display": "none"},
+                    ),
                     html.Button("追加", id="add-button", n_clicks=0),
                 ],
                 style=SIDEBAR_STYLE,
@@ -131,8 +196,30 @@ app.layout = serve_layout()
     State("col-number-dropdown", "value"),
     State("preprocess-dropdown", "value"),
     State("add-field-dropdown", "value"),
+    State("sub-field-dropdown", "value"),
+    State("mul-field-dropdown", "value"),
+    State("shift-field-dropdown", "value"),
+    State("calibration-field-dropdown", "value"),
+    State("moving-average-field-dropdown", "value"),
+    State("regression-line-field-dropdown", "value"),
+    State("thinning-out-field-dropdown", "value"),
 )
-def add_field_to_table(n_clicks, rows, field, row_number, col_number, preprocess, add_field):
+def add_field_to_table(
+    n_clicks,
+    rows,
+    field,
+    row_number,
+    col_number,
+    preprocess,
+    add_field,
+    sub_field,
+    mul_field,
+    shift_field,
+    calibration_field,
+    moving_average_field,
+    regression_line_field,
+    thinning_out_field,
+):
     if n_clicks <= 0:
         return rows
 
@@ -146,6 +233,20 @@ def add_field_to_table(n_clicks, rows, field, row_number, col_number, preprocess
 
     if add_field:
         new_row["detail"] = f"加算行: {add_field}"
+    elif sub_field:
+        new_row["detail"] = f"減算行: {sub_field}"
+    elif mul_field:
+        new_row["detail"] = f"係数: {mul_field}"
+    elif shift_field:
+        new_row["detail"] = f"シフト幅: {shift_field}"
+    elif calibration_field:
+        new_row["detail"] = f"校正: 先頭{calibration_field}件"
+    elif moving_average_field:
+        new_row["detail"] = f"ウィンドウサイズ: {moving_average_field}"
+    elif regression_line_field:
+        new_row["detail"] = f"回帰直線: {regression_line_field}"
+    elif thinning_out_field:
+        new_row["detail"] = f"間引き幅: {thinning_out_field}"
 
     rows.append(new_row)
     return rows
@@ -156,8 +257,92 @@ def add_field_to_table(n_clicks, rows, field, row_number, col_number, preprocess
     Output("add-field-dropdown", "value"),
     Input("preprocess-dropdown", "value"),
 )
-def create_preprocess_detail_dropdown(preprocess):
+def create_add_field_dropdown(preprocess):
     if preprocess == PREPROCESS.ADD.name:
+        return {}, ""
+    else:
+        return {"display": "none"}, ""
+
+
+@app.callback(
+    Output("sub-field", "style"),
+    Output("sub-field-dropdown", "value"),
+    Input("preprocess-dropdown", "value"),
+)
+def create_sub_field_dropdown(preprocess):
+    if preprocess == PREPROCESS.SUB.name:
+        return {}, ""
+    else:
+        return {"display": "none"}, ""
+
+
+@app.callback(
+    Output("mul-field", "style"),
+    Output("mul-field-dropdown", "value"),
+    Input("preprocess-dropdown", "value"),
+)
+def create_mul_field_dropdown(preprocess):
+    if preprocess == PREPROCESS.MUL.name:
+        return {}, ""
+    else:
+        return {"display": "none"}, ""
+
+
+@app.callback(
+    Output("shift-field", "style"),
+    Output("shift-field-dropdown", "value"),
+    Input("preprocess-dropdown", "value"),
+)
+def create_shift_field_dropdown(preprocess):
+    if preprocess == PREPROCESS.SHIFT.name:
+        return {}, ""
+    else:
+        return {"display": "none"}, ""
+
+
+@app.callback(
+    Output("calibration-field", "style"),
+    Output("calibration-field-dropdown", "value"),
+    Input("preprocess-dropdown", "value"),
+)
+def create_calibration_field_dropdown(preprocess):
+    if preprocess == PREPROCESS.CALIBRATION.name:
+        return {}, ""
+    else:
+        return {"display": "none"}, ""
+
+
+@app.callback(
+    Output("moving-average-field", "style"),
+    Output("moving-average-field-dropdown", "value"),
+    Input("preprocess-dropdown", "value"),
+)
+def create_moving_average_field_dropdown(preprocess):
+    if preprocess == PREPROCESS.MOVING_AVERAGE.name:
+        return {}, ""
+    else:
+        return {"display": "none"}, ""
+
+
+@app.callback(
+    Output("regression-line-field", "style"),
+    Output("regression-line-field-dropdown", "value"),
+    Input("preprocess-dropdown", "value"),
+)
+def create_regression_line_field_dropdown(preprocess):
+    if preprocess == PREPROCESS.REGRESSION_LINE.name:
+        return {}, ""
+    else:
+        return {"display": "none"}, ""
+
+
+@app.callback(
+    Output("thinning-out-field", "style"),
+    Output("thinning-out-field-dropdown", "value"),
+    Input("preprocess-dropdown", "value"),
+)
+def create_thinning_out_field_dropdown(preprocess):
+    if preprocess == PREPROCESS.THINNING_OUT.name:
         return {}, ""
     else:
         return {"display": "none"}, ""
@@ -170,8 +355,28 @@ def create_preprocess_detail_dropdown(preprocess):
     State("field-dropdown", "value"),
     State("preprocess-dropdown", "value"),
     State("add-field-dropdown", "value"),
+    State("sub-field-dropdown", "value"),
+    State("mul-field-dropdown", "value"),
+    State("shift-field-dropdown", "value"),
+    State("calibration-field-dropdown", "value"),
+    State("moving-average-field-dropdown", "value"),
+    State("regression-line-field-dropdown", "value"),
+    State("thinning-out-field-dropdown", "value"),
 )
-def set_data(n_clicks, shot_data, field, preprocess, add_field):
+def set_data(
+    n_clicks,
+    shot_data,
+    field,
+    preprocess,
+    add_field,
+    sub_field,
+    mul_field,
+    shift_field,
+    calibration_field,
+    moving_average_field,
+    regression_line_field,
+    thinning_out_field,
+):
     if n_clicks <= 0:
         df = shot_df.copy()
     else:
@@ -184,6 +389,21 @@ def set_data(n_clicks, shot_data, field, preprocess, add_field):
         preprocessed_field = diff(df, field)
     elif preprocess == PREPROCESS.ADD.name:
         preprocessed_field = add(df, field, add_field)
+    elif preprocess == PREPROCESS.SUB.name:
+        preprocessed_field = sub(df, field, sub_field)
+    elif preprocess == PREPROCESS.MUL.name:
+        preprocessed_field = mul(df, field, mul_field)
+    elif preprocess == PREPROCESS.SHIFT.name:
+        preprocessed_field = shift(df, field, shift_field)
+    elif preprocess == PREPROCESS.CALIBRATION.name:
+        preprocessed_field = calibration(df, field, calibration_field)
+    elif preprocess == PREPROCESS.MOVING_AVERAGE.name:
+        preprocessed_field = moving_average(df, field, moving_average_field)
+    elif preprocess == PREPROCESS.REGRESSION_LINE.name:
+        preprocessed_field = regression_line(df, field, regression_line_field)
+        # TODO: モデルから切片と係数を取得してグラフ描写。実装箇所は要検討。
+    elif preprocess == PREPROCESS.THINNING_OUT.name:
+        preprocessed_field = thinning_out(df, field, thinning_out_field)
     else:
         preprocessed_field = df[field]
 
@@ -222,7 +442,7 @@ def add_field_to_graph(shot_data, previous_rows, rows):
                         display_df[row["field"]] = df[row["field"]]
             sub_fig = px.line(data_frame=display_df, x="sequential_number", y=display_df.columns)
             for d in sub_fig.data:
-                fig.add_trace(go.Scatter(x=d["x"], y=d["y"], name=d["name"]), row=m, col=n)
+                fig.add_trace(go.Scatter(x=d["x"], y=d["y"], name=d["name"], connectgaps=True), row=m, col=n)
 
     fig.update_layout(height=500, width=700)
 
