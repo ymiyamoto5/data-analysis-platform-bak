85456621995bedcd0db77ac0b4964f87e614e762
rfukudome2 <ryuga.fukudome@uniadex.co.jp>
2022-08-08 15:24:31 +0900

    *列挙型COREPARAMSを3つに分ける

--- backend/dash_app/constants.py
+++ backend/dash_app/constants.py
@@ -28,14 +28,20 @@ class PREPROCESS(Enum):
     THINNING_OUT = "間引き"
 
 
-class COREPARAMS(Enum):
+class FindType(Enum):
     FIX = "固定"
     RANGE_MORE = "値域>"
     RANGE_LESS = "値域<"
     FEATURE_POINT = "特徴点"
+
+
+class FindTarget(Enum):
     DPT = "DPT"
     VCT = "VCT"
     ACC = "ACC"
+
+
+class FindDir(Enum):
     MAX = "MAX"
     MIN = "MIN"
     RMS = "RMS"
